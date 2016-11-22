d1432b8f4eb1e7c27b99f0508f40e0699b3943ec
sverweij <sverweij@users.noreply.github.com>
2016-11-22 23:01:46 +0100

    changes the dot reporter to cluster modules from one folder

--- src/report/dotReporter.js
+++ src/report/dotReporter.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const path       = require('path');
 const Handlebars = require("handlebars/dist/cjs/handlebars.runtime");
 
 require("./dot.template");
@@ -13,8 +14,27 @@ function compareOnSource(pOne, pTwo) {
     return pOne.source > pTwo.source ? 1 : -1;
 }
 
+function folderify(pDependencyItem) {
+    let lAdditions = {};
+    let lDirName = path.dirname(pDependencyItem.source);
+
+    if (lDirName !== ".") {
+        lAdditions.folder = lDirName;
+    }
+
+    lAdditions.label = path.basename(pDependencyItem.source);
+
+    return Object.assign(
+        pDependencyItem,
+        lAdditions
+    );
+
+}
+
 function render(pInput) {
-    return Handlebars.templates['dot.template.hbs']({"things" : pInput.sort(compareOnSource)});
+    return Handlebars.templates['dot.template.hbs']({
+        "things" : pInput.sort(compareOnSource).map(folderify)
+    });
 }
 
 exports.render = render;
---
+++ test/report.dotReporter.spec.js
@@ -0,0 +1,60 @@
+"use strict";
+const expect = require('chai').expect;
+const render = require('../src/report/dotReporter').render;
+const deps   = require('./fixtures/cjs-no-dependency-valid.json');
+
+const elFixture = `digraph "dependency-cruiser output"{
+    ordering=out
+    rankdir=LR
+    splines=true
+    overlap=false
+    nodesep=0.16
+    fontname="Helvetica"
+    fontsize="9"
+    compound=true
+    node [shape=box style="rounded, filled" fillcolor="#ffffcc" height=0.2 fontname=Helvetica fontsize=9]
+    edge [color=black arrowhead=normal fontname="Helvetica" fontsize="9"]
+
+    subgraph "cluster_node_modules/somemodule/node_modules/someothermodule" {label="node_modules/somemodule/node_modules/someothermodule" style="rounded,filled" fillcolor="#f5f5f5" "node_modules/somemodule/node_modules/someothermodule/main.js" [label="main.js"]}
+    subgraph "cluster_node_modules/somemodule/src" {label="node_modules/somemodule/src" style="rounded,filled" fillcolor="#f5f5f5" "node_modules/somemodule/src/moar-javascript.js" [label="moar-javascript.js"]}
+    subgraph "cluster_node_modules/somemodule/src" {label="node_modules/somemodule/src" style="rounded,filled" fillcolor="#f5f5f5" "node_modules/somemodule/src/somemodule.js" [label="somemodule.js"]}
+    "one_only_one.js"
+    "one_only_two.js"
+    "root_one.js"
+    "root_two.js"
+    "shared.js"
+    subgraph "cluster_sub" {label="sub" style="rounded,filled" fillcolor="#f5f5f5" "sub/depindir.js" [label="depindir.js"]}
+    subgraph "cluster_sub" {label="sub" style="rounded,filled" fillcolor="#f5f5f5" "sub/dir.js" [label="dir.js"]}
+    "two_only_one.js"
+
+    "node_modules/somemodule/src/somemodule.js" -> "node_modules/somemodule/src/moar-javascript.js" [color="red" penwidth=2.0]
+    "node_modules/somemodule/src/somemodule.js" -> "node_modules/somemodule/node_modules/someothermodule/main.js" [color="red" penwidth=2.0]
+    "one_only_one.js" -> "path" [color="red" penwidth=2.0]
+    "one_only_two.js" -> "path" [color="red" penwidth=2.0]
+    "root_one.js" -> "one_only_one.js" [color="red" penwidth=2.0]
+    "root_one.js" -> "one_only_two.js" [color="red" penwidth=2.0]
+    "root_one.js" -> "shared.js" [color="red" penwidth=2.0]
+    "root_one.js" -> "sub/dir.js" [color="red" penwidth=2.0]
+    "root_one.js" -> "fs" [color="red" penwidth=2.0]
+    "root_one.js" -> "node_modules/somemodule/src/somemodule.js" [color="red" penwidth=2.0]
+    "root_two.js" -> "shared.js" [color="red" penwidth=2.0]
+    "root_two.js" -> "somedata.json" [color="red" penwidth=2.0]
+    "root_two.js" -> "two_only_one.js" [color="red" penwidth=2.0]
+    "root_two.js" -> "http" [color="red" penwidth=2.0]
+    "shared.js" -> "path" [color="red" penwidth=2.0]
+    "sub/depindir.js" -> "path" [color="red" penwidth=2.0]
+    "sub/dir.js" -> "sub/depindir.js" [color="red" penwidth=2.0]
+    "sub/dir.js" -> "path" [color="red" penwidth=2.0]
+    "two_only_one.js" -> "sub/dir.js" [color="red" penwidth=2.0]
+}
+`;
+
+describe("dot reporter", () => {
+    it("renders a dot - modules in the root don't come in a cluster", () => {
+        expect(render(deps)).to.deep.equal(elFixture);
+        // console.log(render(deps));
+        // expect(1).to.equal(1);
+    });
+});
+
+/* eslint max-len: 0 */
