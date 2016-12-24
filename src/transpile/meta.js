f30140a3fbd61e90775cc16e8a75468a1eef351c
sverweij <sverweij@users.noreply.github.com>
2016-12-24 23:32:03 +0100

    :nail_care: whitespace-:cop:

--- src/transpile/meta.js
+++ src/transpile/meta.js
@@ -1,22 +1,24 @@
 "use strict";
 
-const jsWrap        = require("./javaScriptWrap");
-const tsWrap        = require("./typeScriptWrap");
-const lsWrap        = require("./liveScriptWrap");
-const coffeeWrap    = require("./coffeeWrap")();
-const litCoffeeWrap = require("./coffeeWrap")(true);
+const javaScriptWrap = require("./javaScriptWrap");
+const typeScriptWrap = require("./typeScriptWrap");
+const liveScriptWrap = require("./liveScriptWrap");
+const coffeeWrap     = require("./coffeeWrap")();
+const litCoffeeWrap  = require("./coffeeWrap")(true);
 
 const extension2wrapper = {
-    ".js"        : jsWrap,
-    ".ts"        : tsWrap,
-    ".d.ts"      : tsWrap,
-    ".ls"        : lsWrap,
+    ".js"        : javaScriptWrap,
+    ".ts"        : typeScriptWrap,
+    ".d.ts"      : typeScriptWrap,
+    ".ls"        : liveScriptWrap,
     ".coffee"    : coffeeWrap,
     ".litcoffee" : litCoffeeWrap,
     ".coffee.md" : litCoffeeWrap
 };
 
-module.exports.getWrapper = pExtension => extension2wrapper[pExtension] || jsWrap;
+module.exports.getWrapper =
+    pExtension => extension2wrapper[pExtension] || javaScriptWrap;
+
 module.exports.allExtensions =
     Object.keys(extension2wrapper)
         .map(
@@ -25,6 +27,7 @@ module.exports.allExtensions =
                 available: extension2wrapper[pKey].isAvailable()
             })
         );
+
 module.exports.scannableExtensions =
     Object.keys(extension2wrapper)
         .filter(
