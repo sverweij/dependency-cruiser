0cf2b9a0f34811a50d5189c4411dbc01598b2b97
sverweij <sverweij@users.noreply.github.com>
2017-03-13 22:53:33 +0100

    :book: adds jsdoc for the internal validation interface

--- src/validate/index.js
+++ src/validate/index.js
@@ -104,6 +104,27 @@ function validateAgainstRules(pRuleSet, pFrom, pTo) {
     return {valid:true};
 }
 
+/**
+ * If pValidate equals true, validates the pFrom and pTo
+ * dependency pair against the given ruleset pRuleSet
+ *
+ * @param  {Boolean} pValidate whether or not to validate at all
+ * @param  {object} pRuleSet  a ruleset (adhering to
+ *                            [the ruleset schema](jsonschema.json))
+ * @param  {object} pFrom     The from part of the dependency
+ * @param  {object} pTo       The 'to' part of the dependency
+ * @return {object}           an object with as attributes:
+ *                            - valid: (boolean) true if the relation
+ *                              between pTo and pFalse is valid (as far as the
+ *                              given ruleset is concerend). false in all other
+ *                              cases.
+ *                            - rule (only when the relation between pFrom and
+ *                              pTo was false):
+ *                              - name: the name (from the ruleset) of the
+ *                                  violated rule
+ *                              - severity: the severity of that rule - as per
+ *                                  the ruleset
+ */
 module.exports = (pValidate, pRuleSet, pFrom, pTo) => {
     if (!pValidate) {
         return {valid:true};
