65dabea3f09ef8ad145f544d27369b1202e2d34e
sverweij <sverweij@users.noreply.github.com>
2018-02-16 09:13:19 +0100

    build(npm): :arrow_up: acorn, chalk, commander, lodash, semver-try-require, coffeescript, eslint, eslint-plugin-node js-makedepend, moocha, nsp, typescript

--- test/extract/transpile/fixtures/csx.jsx
+++ test/extract/transpile/fixtures/csx.jsx
@@ -7,7 +7,7 @@
     {(function() {
       var i, ref, results;
       results = [];
-      for (wholeStar = i = 0, ref = Math.floor(rating); 0 <= ref ? i < ref : i > ref; wholeStar = 0 <= ref ? ++i : --i) {
+      for (wholeStar = i = 0, ref = Math.floor(rating); undefined !== 0 && (0 <= ref ? 0 <= i && i < ref : 0 >= i && i > ref); wholeStar = 0 <= ref ? ++i : --i) {
         results.push(<Star className="wholeStar" key={wholeStar} />);
       }
       return results;
@@ -16,7 +16,7 @@
     {(function() {
       var i, ref, ref1, results;
       results = [];
-      for (emptyStar = i = ref = Math.ceil(rating), ref1 = maxStars; ref <= ref1 ? i < ref1 : i > ref1; emptyStar = ref <= ref1 ? ++i : --i) {
+      for (emptyStar = i = ref = Math.ceil(rating), ref1 = maxStars; undefined !== 0 && (ref <= ref1 ? ref <= i && i < ref1 : ref >= i && i > ref1); emptyStar = ref <= ref1 ? ++i : --i) {
         results.push(<Star className="emptyStar" key={emptyStar} />);
       }
       return results;
