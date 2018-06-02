125c778bae3132ec72f52b1f65527f5474f47845
sverweij <sverweij@users.noreply.github.com>
2018-06-02 19:51:58 +0200

    refactor(extract): break the anonymous call in the core AMD extractor in two functions

--- src/extract/ast-extractors/extract-AMD-deps.js
+++ src/extract/ast-extractors/extract-AMD-deps.js
@@ -3,6 +3,35 @@
 const walk                        = require('acorn/dist/walk');
 const extractCommonJSDependencies = require("./extract-commonJS-deps");
 
+function extractRegularAMDDependencies(pNode, pDependencies) {
+    if (pNode.expression.type === "CallExpression" &&
+        pNode.expression.callee.type === "Identifier" &&
+        (pNode.expression.callee.name === "define" ||
+            pNode.expression.callee.name === "require")) {
+        pNode.expression.arguments
+            .filter(pArg => pArg.type === "ArrayExpression")
+            .forEach(arg => arg.elements.forEach(el => {
+                if (Boolean(el.value) && typeof el.value === "string") {
+                    el.value.split('!').forEach(pString => pDependencies.push({
+                        moduleName: pString,
+                        moduleSystem: "amd"
+                    }));
+                }
+            }));
+    }
+}
+
+function extractCommonJSWrappers(pNode, pDependencies) {
+    if (pNode.expression.type === "CallExpression" &&
+        pNode.expression.callee.type === "Identifier" &&
+        pNode.expression.callee.name === "define") {
+        pNode.expression.arguments
+            .filter(pArg => pArg.type === "FunctionExpression")
+            .filter(pFunction => pFunction.params.some(pParam => pParam.name === "require"))
+            .forEach(pFunction => extractCommonJSDependencies(pFunction.body, pDependencies, "amd"));
+    }
+}
+
 module.exports = (pAST, pDependencies) => {
     walk.simple(
         pAST,
@@ -11,39 +40,14 @@ module.exports = (pAST, pDependencies) => {
                 // module as a function (define(Array, function))
                 // module with a name (define(string, Array, function))
                 // 'root' require module (require(Array, function)
-                if (pNode.expression.type === "CallExpression" &&
-                     pNode.expression.callee.type === "Identifier" &&
-                     (pNode.expression.callee.name === "define" ||
-                       pNode.expression.callee.name === "require")){
-                    pNode.expression.arguments
-                        .filter(pArg => pArg.type === "ArrayExpression")
-                        .forEach(arg =>
-                            arg.elements.forEach(el => {
-                                if (Boolean(el.value) && typeof el.value === "string") {
-                                    el.value.split('!').forEach(pString =>
-                                        pDependencies.push({
-                                            moduleName: pString,
-                                            moduleSystem: "amd"
-                                        })
-                                    );
-                                }
-                            })
-                        );
-                }
+                extractRegularAMDDependencies(pNode, pDependencies);
                 // CommonJS-wrappers:
                 //  (define(function(require, exports, module){
                 //  define(["require", ...], function(require, ...){
                 //      ... every 'require' call is a depencency
                 // Won't work if someone decides to name the first parameter of
                 // the function passed to the define something else from "require"
-                if (pNode.expression.type === "CallExpression" &&
-                     pNode.expression.callee.type === "Identifier" &&
-                     pNode.expression.callee.name === "define") {
-                    pNode.expression.arguments
-                        .filter(pArg => pArg.type === "FunctionExpression")
-                        .filter(pFunction => pFunction.params.some(pParam => pParam.name === "require"))
-                        .forEach(pFunction => extractCommonJSDependencies(pFunction.body, pDependencies, "amd"));
-                }
+                extractCommonJSWrappers(pNode, pDependencies);
             }
         }
     );
