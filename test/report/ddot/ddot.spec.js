92a3c1a158fe41555542164a2621563de4e50c1f
sverweij <sverweij@users.noreply.github.com>
2019-01-15 22:21:58 +0100

    test(report): add a slightly bigger test case to the ddot reporter test

--- test/report/ddot/ddot.spec.js
+++ test/report/ddot/ddot.spec.js
@@ -3,9 +3,11 @@ const expect  = require('chai').expect;
 const render  = require('../../../src/report/ddot');
 const deps    = require('./fixtures/dependency-cruiser-2019-01-14.json');
 const orphans = require('./fixtures/orphans.json');
+const rxjs    = require('./fixtures/rxjs.json');
 
 const consolidatedDot        = fs.readFileSync('test/report/ddot/fixtures/dependency-cruiser-2019-01-14.dot', 'utf8');
 const consolidatedOrphansDot = fs.readFileSync('test/report/ddot/fixtures/consolidated-orphans.dot', 'utf8');
+const consolidatedRxJs       = fs.readFileSync('test/report/ddot/fixtures/consolidated-rxjs.dot', 'utf8');
 
 describe("ddot reporter", () => {
     it("consolidates to folder level", () => {
@@ -14,6 +16,10 @@ describe("ddot reporter", () => {
 
     it("consolidates module only transgressions correctly", () => {
         expect(render(orphans).modules).to.deep.equal(consolidatedOrphansDot);
+    });
+
+    it("consolidates a slightly larger code base in a timely fashion", () => {
+        expect(render(rxjs).modules).to.deep.equal(consolidatedRxJs);
     });
 });
 
