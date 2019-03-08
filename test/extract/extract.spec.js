f0ef902fd1263dbc42071e67f7c98012123412c5
sverweij <sverweij@users.noreply.github.com>
2019-03-08 20:58:05 +0100

    fix(test): work around symlink-dir's node 6 desupport - simpler solution
    just donotup

--- test/extract/extract.spec.js
+++ test/extract/extract.spec.js
@@ -1,6 +1,6 @@
 const path              = require('path');
 const fs                = require('fs');
-
+const symlinkDir        = require('symlink-dir');
 const expect            = require('chai').expect;
 const extract           = require('../../src/extract/extract');
 const normalize         = require('../../src/main/options/normalize');
@@ -43,131 +43,117 @@ function runFixture(pFixture) {
     });
 }
 
-/* so symlink-dir decided to desupport node 6 before node 6 is
-    actually out of maintenance (and use fancy-pancy javascript
-    that only works in v8+). But we still support node 6 until
-    the end of maintenance schedule (APR2019).
-    This wonky if statement makes sure that tests that need
-    symlink-dir don't run on node 6.
+/* eslint-disable mocha/no-top-level-hooks */
+before((cb) => {
+    symlinkDir(path.join(__dirname, 'fixtures', 'symlinkTarget'), symlinkDirectory)
+        .then(() => cb(), (err) => cb(err));
+});
 
-*/
-if (process.version.split('.').shift() !== 'v6') {
+after(() => {
+    try {
+        fs.unlinkSync(symlinkDirectory);
+    } catch (e) {
+        // just swallow the error, there's nothing we can do about it
+    }
+});
 
-    /* eslint global-require:0 */
-    const symlinkDir = require('symlink-dir');
+describe('extract/extract - CommonJS - ', () => cjsFixtures.forEach(runFixture));
+describe('extract/extract - CommonJS - with bangs', () => {
 
-    /* eslint-disable mocha/no-top-level-hooks */
-    before((cb) => {
-        symlinkDir(path.join(__dirname, 'fixtures', 'symlinkTarget'), symlinkDirectory)
-            .then(() => cb(), (err) => cb(err));
+    it('splits bang!./blabla into bang and ./blabla', () => {
+        expect(
+            extract(
+                "test/extract/fixtures/cjs-bangs/index.js",
+                normalize({
+                    moduleSystems: ["cjs"]
+                }),
+                {}
+            )
+        ).to.deep.equal(
+            cjsBang
+        );
+    });
+});
+
+describe('extract/extract - ES6 - ', () => es6Fixtures.forEach(runFixture));
+describe('extract/extract - AMD - ', () => amdFixtures.forEach(runFixture));
+describe('AMD - with bangs', () => {
+
+    it('splits bang!./blabla into bang and ./blabla - regular requirejs', () => {
+        expect(
+            extract(
+                "test/extract/fixtures/amd-bangs/root_one.js",
+                normalize({
+                    moduleSystems: ["amd"]
+                }),
+                {}
+            )
+        ).to.deep.equal(
+            amdBangRequirejs
+        );
     });
 
-    after(() => {
-        try {
-            fs.unlinkSync(symlinkDirectory);
-        } catch (e) {
-            // just swallow the error, there's nothing we can do about it
-        }
+    it('splits bang!./blabla into bang and ./blabla - CommonJS wrapper', () => {
+        expect(
+            extract(
+                "test/extract/fixtures/amd-bangs/simplified-commonjs-wrapper.js",
+                normalize({
+                    moduleSystems: ["amd"]
+                }),
+                {}
+            )
+        ).to.deep.equal(
+            amdBangCJSWrapper
+        );
+    });
+});
+
+describe('extract/extract - TypeScript - ', () => tsFixtures.forEach(runFixture));
+describe('extract/extract - CoffeeScript - ', () => coffeeFixtures.forEach(runFixture));
+
+describe('extract/extract - Error scenarios - ', () => {
+    it('Does not raise an exception on syntax errors (because we\'re on the loose parser)', () => {
+        expect(
+            () => extract("test/extract/fixtures/syntax-error.js", normalize({}), {})
+        ).to.not.throw("Extracting dependencies ran afoul of... Unexpected token (1:3)");
+    });
+    it('Raises an exception on non-existing files', () => {
+        expect(
+            () => extract("non-existing-file.md", normalize({}), {})
+        ).to.throw(
+            "Extracting dependencies ran afoul of...\n\n  ENOENT: no such file or directory, open "
+        );
+    });
+});
+
+describe('extract/extract - even when require gets non-string arguments, extract doesn\'t break', () => {
+    it('Just skips require(481)', () => {
+        expect(
+            extract(
+                "./test/extract/fixtures/cjs-require-non-strings/require-a-number.js",
+                normalize({}),
+                {}
+            ).length
+        ).to.equal(1);
     });
 
-    describe('extract/extract - CommonJS - ', () => cjsFixtures.forEach(runFixture));
-    describe('extract/extract - CommonJS - with bangs', () => {
-
-        it('splits bang!./blabla into bang and ./blabla', () => {
-            expect(
-                extract(
-                    "test/extract/fixtures/cjs-bangs/index.js",
-                    normalize({
-                        moduleSystems: ["cjs"]
-                    }),
-                    {}
-                )
-            ).to.deep.equal(
-                cjsBang
-            );
-        });
+    it('Just skips require(a function)', () => {
+        expect(
+            extract(
+                "./test/extract/fixtures/cjs-require-non-strings/require-a-function.js",
+                normalize({}),
+                {}
+            ).length
+        ).to.equal(1);
     });
 
-    describe('extract/extract - ES6 - ', () => es6Fixtures.forEach(runFixture));
-    describe('extract/extract - AMD - ', () => amdFixtures.forEach(runFixture));
-    describe('AMD - with bangs', () => {
-
-        it('splits bang!./blabla into bang and ./blabla - regular requirejs', () => {
-            expect(
-                extract(
-                    "test/extract/fixtures/amd-bangs/root_one.js",
-                    normalize({
-                        moduleSystems: ["amd"]
-                    }),
-                    {}
-                )
-            ).to.deep.equal(
-                amdBangRequirejs
-            );
-        });
-
-        it('splits bang!./blabla into bang and ./blabla - CommonJS wrapper', () => {
-            expect(
-                extract(
-                    "test/extract/fixtures/amd-bangs/simplified-commonjs-wrapper.js",
-                    normalize({
-                        moduleSystems: ["amd"]
-                    }),
-                    {}
-                )
-            ).to.deep.equal(
-                amdBangCJSWrapper
-            );
-        });
+    it('Just skips require(an iife)', () => {
+        expect(
+            extract(
+                "./test/extract/fixtures/cjs-require-non-strings/require-an-iife.js",
+                normalize({}),
+                {}
+            ).length
+        ).to.equal(1);
     });
-
-    describe('extract/extract - TypeScript - ', () => tsFixtures.forEach(runFixture));
-    describe('extract/extract - CoffeeScript - ', () => coffeeFixtures.forEach(runFixture));
-
-    describe('extract/extract - Error scenarios - ', () => {
-        it('Does not raise an exception on syntax errors (because we\'re on the loose parser)', () => {
-            expect(
-                () => extract("test/extract/fixtures/syntax-error.js", normalize({}), {})
-            ).to.not.throw("Extracting dependencies ran afoul of... Unexpected token (1:3)");
-        });
-        it('Raises an exception on non-existing files', () => {
-            expect(
-                () => extract("non-existing-file.md", normalize({}), {})
-            ).to.throw(
-                "Extracting dependencies ran afoul of...\n\n  ENOENT: no such file or directory, open "
-            );
-        });
-    });
-
-    describe('extract/extract - even when require gets non-string arguments, extract doesn\'t break', () => {
-        it('Just skips require(481)', () => {
-            expect(
-                extract(
-                    "./test/extract/fixtures/cjs-require-non-strings/require-a-number.js",
-                    normalize({}),
-                    {}
-                ).length
-            ).to.equal(1);
-        });
-
-        it('Just skips require(a function)', () => {
-            expect(
-                extract(
-                    "./test/extract/fixtures/cjs-require-non-strings/require-a-function.js",
-                    normalize({}),
-                    {}
-                ).length
-            ).to.equal(1);
-        });
-
-        it('Just skips require(an iife)', () => {
-            expect(
-                extract(
-                    "./test/extract/fixtures/cjs-require-non-strings/require-an-iife.js",
-                    normalize({}),
-                    {}
-                ).length
-            ).to.equal(1);
-        });
-    });
-}
+});
