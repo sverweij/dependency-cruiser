32989f0742fc80a6e0f386deca15086242f53787
sverweij <sverweij@users.noreply.github.com>
2019-03-17 20:37:06 +0100

    feature(typescript): adds commonjs extraction to the typescript AST extractor

--- test/extract/ast-extractors/extract-typescript-commonjs.spec.js
+++ test/extract/ast-extractors/extract-typescript-commonjs.spec.js
@@ -15,4 +15,71 @@ describe("ast-extractors/extract-typescript - regular commonjs require", () => {
             ]
         );
     });
+
+    it("extracts regular require as a const, let or var", () => {
+        expect(
+            extractTypescript(
+                `const lala1 = require('legit-one');
+                 let lala2 = require('legit-two');
+                 var lala3 = require('legit-three');`
+            )
+        ).to.deep.equal(
+            [
+                {
+                    moduleName: 'legit-one',
+                    moduleSystem: 'cjs'
+                },
+                {
+                    moduleName: 'legit-two',
+                    moduleSystem: 'cjs'
+                },
+                {
+                    moduleName: 'legit-three',
+                    moduleSystem: 'cjs'
+                }
+            ]
+        );
+    });
+
+    it("extracts regular requires that are not on the top level in the AST", () => {
+        expect(
+            extractTypescript(
+                `function f(x) {
+                    if(x > 0) {
+                        return require('midash')
+                    } else {
+                        const hi = require('slodash').splut();
+                        for (i=0;i<10;i++) {
+                            if (hi(i)) {
+                                return require('hidash');
+                            }
+                        }
+                    }
+                }`
+            )
+        ).to.deep.equal(
+            [{
+                moduleName: 'midash',
+                moduleSystem: 'cjs'
+            },
+            {
+                moduleName: 'slodash',
+                moduleSystem: 'cjs'
+            },
+            {
+                moduleName: 'hidash',
+                moduleSystem: 'cjs'
+            }]
+        );
+    });
+
+    it("ignores regular require without parameters", () => {
+        expect(
+            extractTypescript(
+                "const lala = require()"
+            )
+        ).to.deep.equal(
+            []
+        );
+    });
 });
