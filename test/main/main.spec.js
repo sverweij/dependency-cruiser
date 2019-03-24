8b35807a76c5536904dc542050839be4d915323b
sverweij <sverweij@users.noreply.github.com>
2019-03-24 17:04:08 +0100

    test(extract): ensure that both with ts precompilation deps on and off things keep working

--- test/main/main.spec.js
+++ test/main/main.spec.js
@@ -4,6 +4,10 @@ const depSchema  = require('../../src/extract/jsonschema.json');
 const tsFixture  = require('./fixtures/ts.json');
 const tsxFixture = require('./fixtures/tsx.json');
 const jsxFixture = require('./fixtures/jsx.json');
+const tsPreCompFixtureCJS = require('./fixtures/ts-precomp-cjs.json');
+const tsPreCompFixtureES = require('./fixtures/ts-precomp-es.json');
+const tsNoPrecompFixtureCJS = require('./fixtures/ts-no-precomp-cjs.json');
+const tsNoPrecompFixtureES = require('./fixtures/ts-no-precomp-es.json');
 
 const expect     = chai.expect;
 
@@ -23,13 +27,13 @@ describe("main", () => {
         expect(lResult).to.be.jsonSchema(depSchema);
     });
     it("Also processes tsx correctly", () => {
-        const lResult = main.cruise(["test/main/fixtures/tsx"]);
+        const lResult = main.cruise(["test/main/fixtures/tsx"], {}, {bustTheCache:true});
 
         expect(lResult).to.deep.equal(tsxFixture);
         expect(lResult).to.be.jsonSchema(depSchema);
     });
     it("And jsx", () => {
-        const lResult = main.cruise(["test/main/fixtures/jsx"]);
+        const lResult = main.cruise(["test/main/fixtures/jsx"], {}, {bustTheCache:true});
 
         expect(lResult).to.deep.equal(jsxFixture);
         expect(lResult).to.be.jsonSchema(depSchema);
@@ -39,10 +43,95 @@ describe("main", () => {
             ["test/main/fixtures/jsx"],
             {
                 ruleSet : {}
-            }
+            },
+            {bustTheCache:true}
         );
 
         expect(lResult).to.deep.equal(jsxFixture);
+        expect(lResult).to.be.jsonSchema(depSchema);
+    });
+    it("ts-pre-compilation-deps: on, target CJS", () => {
+        const lResult = main.cruise(
+            ["test/main/fixtures/ts-precompilation-deps-on-cjs"],
+            {
+                tsConfig: {
+                    fileName: "test/main/fixtures/tsconfig.targetcjs.json"
+                },
+                tsPreCompilationDeps: true
+            },
+            {bustTheCache:true},
+            {
+                "options": {
+                    "baseUrl": ".",
+                    "module": "commonjs"
+                }
+            }
+        );
+
+        expect(lResult).to.deep.equal(tsPreCompFixtureCJS);
+        expect(lResult).to.be.jsonSchema(depSchema);
+    });
+    it("ts-pre-compilation-deps: on, target ES", () => {
+        const lResult = main.cruise(
+            ["test/main/fixtures/ts-precompilation-deps-on-es"],
+            {
+                tsConfig: {
+                    fileName: "test/main/fixtures/tsconfig.targetes.json"
+                },
+                tsPreCompilationDeps: true
+            },
+            {bustTheCache:true},
+            {
+                "options": {
+                    "baseUrl": ".",
+                    "module": "es6"
+                }
+            }
+        );
+
+        expect(lResult).to.deep.equal(tsPreCompFixtureES);
+        expect(lResult).to.be.jsonSchema(depSchema);
+    });
+    it("ts-pre-compilation-deps: off, target CJS", () => {
+        const lResult = main.cruise(
+            ["test/main/fixtures/ts-precompilation-deps-off-cjs"],
+            {
+                tsConfig: {
+                    fileName: "test/main/fixtures/tsconfig.targetcjs.json"
+                },
+                tsPreCompilationDeps: false
+            },
+            {bustTheCache:true},
+            {
+                "options": {
+                    "baseUrl": ".",
+                    "module": "commonjs"
+                }
+            }
+        );
+
+        expect(lResult).to.deep.equal(tsNoPrecompFixtureCJS);
+        expect(lResult).to.be.jsonSchema(depSchema);
+    });
+    it("ts-pre-compilation-deps: off, target ES", () => {
+        const lResult = main.cruise(
+            ["test/main/fixtures/ts-precompilation-deps-off-es"],
+            {
+                tsConfig: {
+                    fileName: "test/main/fixtures/tsconfig.targetes.json"
+                },
+                tsPreCompilationDeps: false
+            },
+            {bustTheCache:true},
+            {
+                "options": {
+                    "baseUrl": ".",
+                    "module": "es6"
+                }
+            }
+        );
+
+        expect(lResult).to.deep.equal(tsNoPrecompFixtureES);
         expect(lResult).to.be.jsonSchema(depSchema);
     });
 });
