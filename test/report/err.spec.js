39dc20406d76f833fecc955bcb0bc33e10e55a54
Sander Verweij <sverweij@users.noreply.github.com>
2019-06-08 20:57:05 +0200

    feature(report): err, teamcity: for module-only rule violations emit the module name once (#143)

--- test/report/err.spec.js
+++ test/report/err.spec.js
@@ -1,19 +1,40 @@
-const expect   = require('chai').expect;
-const render   = require('../../src/report/err');
-const okdeps   = require('./fixtures/everything-fine.json');
-const deps     = require('./fixtures/cjs-no-dependency-valid.json');
-const warndeps = require('./fixtures/err-only-warnings.json');
-const erradds  = require('./fixtures/err-with-additional-information.json');
+const expect     = require('chai').expect;
+const chalk      = require('chalk');
+const render     = require('../../src/report/err');
+const okdeps     = require('./fixtures/everything-fine.json');
+const deps       = require('./fixtures/cjs-no-dependency-valid.json');
+const warndeps   = require('./fixtures/err-only-warnings.json');
+const erradds    = require('./fixtures/err-with-additional-information.json');
+const orphanerrs = require('./fixtures/orphan-deps.json');
 
 describe("report/err", () => {
+    let chalkEnabled = chalk.enabled;
+
+    before("disable chalk coloring", () => {
+        chalk.enabled = false;
+    });
+    after("put chalk enabled back to its original value", () => {
+        chalk.enabled = chalkEnabled;
+    });
     it("says everything fine", () => {
         expect(render(okdeps)).to.contain('no dependency violations found');
     });
     it("renders a bunch of errors", () => {
-        expect(render(deps)).to.contain('2 dependency violations (2 errors, 0 warnings)');
+        const lResult = render(deps);
+
+        expect(lResult).to.contain(
+            'error no-leesplank: aap → noot\n'
+        );
+        expect(lResult).to.contain('2 dependency violations (2 errors, 0 warnings)');
     });
     it("renders a bunch of warnings", () => {
         expect(render(warndeps)).to.contain('1 dependency violations (0 errors, 1 warnings)');
+    });
+    it("renders module only violations as module only", () => {
+        const lResult = render(orphanerrs);
+
+        expect(lResult).to.contain('error no-orphans: remi.js\n');
+        expect(lResult).to.contain('1 dependency violations (1 errors, 0 warnings)');
     });
     it("renders addtional information", () => {
         const lResult = render(erradds);
