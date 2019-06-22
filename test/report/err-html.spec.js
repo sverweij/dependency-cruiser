3a6364d99e1d9e6ba3e26d703d05be5e92f44f97
sverweij <sverweij@users.noreply.github.com>
2019-06-22 21:26:57 +0200

    test(report): add some more contents checks to the err-html reporter test

--- test/report/err-html.spec.js
+++ test/report/err-html.spec.js
@@ -5,20 +5,27 @@ const validationMoreThanOnce  = require('./fixtures/violation-more-than-once.jso
 
 
 describe("report/err-html", () => {
-    it('lalala everything fine', () => {
-        const lOkeliDokelyKey = 'gummy bears';
+    const lOkeliDokelyKey = 'gummy bears';
 
+    it('lalala everything fine', () => {
         expect(errHTML(everythingFineResult)).to.contain(lOkeliDokelyKey);
     });
 
     it('lalala everything not fine', () => {
-        const lOkeliDokelyKey = 'gummy bears';
         const lReport = errHTML(validationMoreThanOnce);
 
         expect(lReport).to.not.contain(lOkeliDokelyKey);
         expect(lReport).to.contain('All violations');
+        expect(lReport).to.contain('<strong>127</strong> modules');
+        expect(lReport).to.contain('<strong>259</strong> dependencies');
+        expect(lReport).to.contain('<strong>0</strong> errors');
+        expect(lReport).to.contain('<strong>1</strong> warnings');
+        expect(lReport).to.contain('<strong>2</strong> informational');
 
-
+        expect(lReport).to.contain('<td><strong>2</strong></td>');
+        expect(lReport).to.contain(
+            '<a href="https://github.com/sverweij/dependency-cruiser/blob/develop/src/cli/compileConfig/index.js">'
+        );
     });
 
 });
