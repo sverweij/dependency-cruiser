6e4dbc215b0d6319b2deaf941e3a034ddd750b4f
sverweij <sverweij@users.noreply.github.com>
2019-06-23 12:34:06 +0200

    refactor(main): split off the constants from the resolution compilation step

--- src/main/resolveOptions/normalize.js
+++ src/main/resolveOptions/normalize.js
@@ -6,51 +6,50 @@ const TsConfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 const transpileMeta       = require('../../extract/transpile/meta');
 
 const CACHE_DURATION = 4000;
+const DEFAULT_RESOLVE_OPTIONS = {
+    // for later: check semantics of enhanced-resolve symlinks and
+    // node's preserveSymlinks. They seem to be
+    // symlink === !preserveSynlinks - but using it that way
+    // breaks backwards compatibility
+    //
+    // Someday we'll rely on this and remove the code that manually
+    // does this in extract/resolve/index.js
+    symlinks: false,
+    // if a webpack config overrides extensions, there's probably
+    // good cause. The scannableExtensions are an educated guess
+    // anyway, that works well in most circumstances.
+    // Note that if extract/transpile/index gets an unknown extension
+    // passed, it'll fall back to the javascript parser
+    extensions: transpileMeta.scannableExtensions,
+    // for typescript projects that import stuff that's only in
+    // node_modules/@types we need:
+    // - the inclusion of .d.ts to the extensions (see above)
+    // - an explicit inclusion of node_modules/@types to the spots
+    //   to look for modules (in addition to "node_modules" which
+    //   is the default for enhanced-resolve)
+    modules: ["node_modules", "node_modules/@types"]
+};
+
+const NON_OVERRIDABLE_RESOLVE_OPTIONS = {
+    // This should cover most of the bases of dependency-cruiser's
+    // uses. Not overridable for now because for other
+    // file systems it's not sure we can use sync system calls
+    // Also: passing a non-cached filesystem makes performance
+    // worse.
+    fileSystem: new enhancedResolve.CachedInputFileSystem(
+        new enhancedResolve.NodeJsInputFileSystem(),
+        CACHE_DURATION
+    ),
+    // our code depends on sync behavior, so having this
+    // overriden is not an option
+    useSyncFileSystemCalls: true
+};
 
 function pushPlugin(pPlugins, pPluginToPush) {
     return (pPlugins || []).concat(pPluginToPush);
 }
 
 function compileResolveOptions(pResolveOptions, pTSConfig){
-    let DEFAULT_RESOLVE_OPTIONS = {
-        // for later: check semantics of enhanced-resolve symlinks and
-        // node's preserveSymlinks. They seem to be
-        // symlink === !preserveSynlinks - but using it that way
-        // breaks backwards compatibility
-        //
-        // Someday we'll rely on this and remove the code that manually
-        // does this in extract/resolve/index.js
-        symlinks: false,
-        // if a webpack config overrides extensions, there's probably
-        // good cause. The scannableExtensions are an educated guess
-        // anyway, that works well in most circumstances.
-        // Note that if extract/transpile/index gets an unknown extension
-        // passed, it'll fall back to the javascript parser
-        extensions: transpileMeta.scannableExtensions,
-        // for typescript projects that import stuff that's only in
-        // node_modules/@types we need:
-        // - the inclusion of .d.ts to the extensions (see above)
-        // - an explicit inclusion of node_modules/@types to the spots
-        //   to look for modules (in addition to "node_modules" which
-        //   is the default for enhanced-resolve)
-        modules: ["node_modules", "node_modules/@types"]
-    };
-
-    const NON_OVERRIDABLE_RESOLVE_OPTIONS = {
-        // This should cover most of the bases of dependency-cruiser's
-        // uses. Not overridable for now because for other
-        // file systems it's not sure we can use sync system calls
-        // Also: passing a non-cached filesystem makes performance
-        // worse.
-        fileSystem: new enhancedResolve.CachedInputFileSystem(
-            new enhancedResolve.NodeJsInputFileSystem(),
-            CACHE_DURATION
-        ),
-        // our code depends on sync behavior, so having this
-        // overriden is not an option
-        useSyncFileSystemCalls: true
-    };
-
     let lResolveOptions = {};
 
     // TsConfigPathsPlugin requires a baseUrl to be present in the
@@ -74,6 +73,7 @@ function compileResolveOptions(pResolveOptions, pTSConfig){
     }
 
     return Object.assign(
+        {},
         DEFAULT_RESOLVE_OPTIONS,
         lResolveOptions,
         pResolveOptions,
