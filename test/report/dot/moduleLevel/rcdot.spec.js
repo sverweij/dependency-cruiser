193c8b7a4ec342824b8115b346605d96b92c6294
sverweij <sverweij@users.noreply.github.com>
2019-06-30 08:44:03 +0200

    refactor(report): simplify the dot reporter interface

--- test/report/dot/moduleLevel/rcdot.spec.js
+++ test/report/dot/moduleLevel/rcdot.spec.js
@@ -1,10 +1,9 @@
 const fs              = require('fs');
 const expect          = require('chai').expect;
 const RCScheme        = require('../../../../src/report/dot/common/richModuleColorScheme.json');
-const renderRCDot     = require('../../../../src/report/dot/moduleLevel')(RCScheme);
-const renderDot       = require('../../../../src/report/dot/moduleLevel')();
+const renderDot       = require('../../../../src/report/dot/moduleLevel');
 const deps            = require('../../fixtures/richmodulecolor.json');
-const renderBoringDot = require('../../../../src/report/dot/moduleLevel')(require('./boringModuleColorScheme.json'));
+const boringScheme    = require('./boringModuleColorScheme.json');
 
 
 const richColorFixture = fs.readFileSync('test/report/fixtures/richmodulecolor.dot', 'utf8');
@@ -12,12 +11,12 @@ const boringColorFixture = fs.readFileSync('test/report/fixtures/defaultmoduleco
 
 describe("report/dot reporter coloring", () => {
     it("richly colors when passed a rich color scheme", () => {
-        expect(renderRCDot(deps)).to.deep.equal(richColorFixture);
+        expect(renderDot(deps, RCScheme)).to.deep.equal(richColorFixture);
     });
     it("defaultly colors when passed no color scheme", () => {
         expect(renderDot(deps)).to.deep.equal(richColorFixture);
     });
     it("colors boringly when passed the boring color scheme", () => {
-        expect(renderBoringDot(deps)).to.deep.equal(boringColorFixture);
+        expect(renderDot(deps, boringScheme)).to.deep.equal(boringColorFixture);
     });
 });
