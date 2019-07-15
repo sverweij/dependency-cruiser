937c2dbf48791a54c8a42655933d41a6245b78fa
Sander Verweij <sverweij@users.noreply.github.com>
2019-07-15 21:34:23 +0200

    bugfix(resolve): reverts to resolve.isCore for detecting core modules (#174)
    Use resolve.isCore instead of core functionality to detect the whether a module is core as some (still) supported node versions (elder 8 and 6 - newer 8 and 6 do have it!) donot have this functionality in the module module - as discovered in #167.

--- src/extract/resolve/isCore.js
+++ src/extract/resolve/isCore.js
@@ -1,3 +1,5 @@
-const builtinModules = require('module').builtinModules;
+// const builtinModules = require('module').builtinModules;
+const resolve = require('resolve');
 
-module.exports = (pModuleName) => builtinModules.some(pBuiltinModule => pBuiltinModule === pModuleName);
+module.exports = resolve.isCore;
+// module.exports = (pModuleName) => builtinModules.some(pBuiltinModule => pBuiltinModule === pModuleName);
