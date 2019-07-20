5f01499ccd0e0d35fe27bd41cf57309e98e1f65c
sverweij <sverweij@users.noreply.github.com>
2019-07-20 16:30:11 +0200

    doc(cli): copy update to validateNodEnv error message
    [ci skip]

--- src/cli/validateNodeEnv.js
+++ src/cli/validateNodeEnv.js
@@ -5,7 +5,7 @@ module.exports = function validateNodeEnv(pNodeVersion) {
     // not using default parameter here because the check should run
     // run on node 4 as well
     const lNodeVersion = pNodeVersion || process.versions.node;
-    const VERSION_ERR = `\nERROR: This node version (${lNodeVersion}) is not supported. dependency-cruiser
+    const VERSION_ERR = `\nERROR: Your node version (${lNodeVersion}) is not supported. dependency-cruiser
        follows the node.js release cycle and runs on these node versions:
        ${$package.engines.node}
        See https://nodejs.org/en/about/releases/ for details.
