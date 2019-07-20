00a213b592bb9bf784968322a1748a6ca1b03125
sverweij <sverweij@users.noreply.github.com>
2019-07-20 16:19:07 +0200

    refactor(cli): put current node version in an argument to validateNodeEnv
    so callers need less knowledge of inner working

--- src/cli/validateNodeEnv.js
+++ src/cli/validateNodeEnv.js
@@ -2,14 +2,17 @@ const semver   = require("semver");
 const $package = require("../../package.json");
 
 module.exports = function validateNodeEnv(pNodeVersion) {
-    const VERSION_ERR = `\nERROR: This node version (${pNodeVersion}) is not supported. dependency-cruiser
+    // not using default parameter here because the check should run
+    // run on node 4 as well
+    const lNodeVersion = pNodeVersion || process.versions.node;
+    const VERSION_ERR = `\nERROR: This node version (${lNodeVersion}) is not supported. dependency-cruiser
        follows the node.js release cycle and runs on these node versions:
        ${$package.engines.node}
        See https://nodejs.org/en/about/releases/ for details.
 
 `;
 
-    if (!semver.satisfies(pNodeVersion, $package.engines.node)) {
+    if (!semver.satisfies(lNodeVersion, $package.engines.node)) {
         throw new Error(VERSION_ERR);
     }
 };
--- test/cli/valideNodeEnv.spec.js
+++ test/cli/valideNodeEnv.spec.js
@@ -2,20 +2,31 @@ const expect        = require("chai").expect;
 const validateNodeEnv = require("../../src/cli/validateNodeEnv");
 
 describe("cli/validateNodeEnv", () => {
-    it("throws when no node version is passed", () => {
-        expect(() => validateNodeEnv()).to.throw();
-    });
 
-    it("throws when an empty node version is passed", () => {
-        expect(() => validateNodeEnv("")).to.throw();
-    });
-
-    it("doesn't throw when an older and unsupported node version is passed", () => {
+    it("throws when an older and unsupported node version is passed", () => {
         expect(() => validateNodeEnv("6.0.0")).to.throw();
     });
 
-    it("doesn't throw when a newer but unsupported node version is passed", () => {
+    it("throws when a newer but unsupported node version is passed", () => {
         expect(() => validateNodeEnv("9.0.0")).to.throw();
+    });
+
+    it("doesn't throw when an empty node version is passed (assuming test is run on a supported platform)", () => {
+        expect(() => validateNodeEnv("")).to.not.throw();
+    });
+
+    it("doesn't throw when a null node version is passed (assuming test is run on a supported platform)", () => {
+        // eslint-disable-next-line no-undefined
+        expect(() => validateNodeEnv(null)).to.not.throw();
+    });
+
+    it("doesn't throw when an undefined node version is passed (assuming test is run on a supported platform)", () => {
+        // eslint-disable-next-line no-undefined
+        expect(() => validateNodeEnv(undefined)).to.not.throw();
+    });
+
+    it("doesn't throw when no node version is passed (assuming this test is run on a supported platform ...)", () => {
+        expect(() => validateNodeEnv()).to.not.throw();
     });
 
     it("doesn't throw when a supported node version is passed", () => {
