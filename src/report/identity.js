4e5a53051515e4c2ab2bf1f9f912be9111f57184
sverweij <sverweij@users.noreply.github.com>
2019-07-20 13:03:46 +0200

    doc(report): document the identity reporter
    [ci skip]

--- src/report/identity.js
+++ src/report/identity.js
@@ -1,3 +1,10 @@
+/**
+ * Returns the results of a cruise in a javascript object
+ *
+ * @param {any} pResults - the output of a dependency-cruise adhering to ../extract/results-schema.json
+ * @returns {object} - output: the results in a javascript object (hence adhering to the same json schema)
+ *                     exitCode: 0
+ */
 module.exports = (pResults) => (
     {
         output: pResults,
