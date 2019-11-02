3ff15a21999f82a8d982d3c6f75602f0a844ee12
sverweij <sverweij@users.noreply.github.com>
2019-11-02 13:42:01 +0100

    keep the OUTFILE and the removeDammit function within the scope of the describe
    [ci skip]

--- test/cli/utl/io.spec.js
+++ test/cli/utl/io.spec.js
@@ -5,30 +5,30 @@ const stream = require("stream");
 const expect = require("chai").expect;
 const getInStream = require("../../../src/cli/utl/io").getInStream;
 
-const OUTFILE = path.join(
-  __dirname,
-  "output",
-  `tmp_hello_${Math.random()
-    .toString()
-    .substr("0.".length)}.json`
-);
-
-const removeDammit = pFileName => {
-  try {
-    process.stdout.write(`|>|>|>|> unlinkin ${pFileName} starting\n`);
-    fs.unlinkSync(pFileName);
-    process.stdout.write(`|>|>|>|> unlinkin ${pFileName} done\n`);
-  } catch (e) {
-    // probably files didn't exist in the first place
-    // so ignore the exception
-    process.stdout.write(`|>|>|>|> could not unlink ${pFileName} - ${e}\n`);
-  } finally {
-    // explicitly ignore finally as well
-    process.stdout.write(`|>|>|>|> finally!\n`);
-  }
-};
-
 describe("cli/utl/io", () => {
+  const OUTFILE = path.join(
+    __dirname,
+    "output",
+    `tmp_hello_${Math.random()
+      .toString()
+      .substr("0.".length)}.json`
+  );
+
+  function removeDammit(pFileName) {
+    try {
+      process.stdout.write(`|>|>|>|> unlinkin ${pFileName} starting\n`);
+      fs.unlinkSync(pFileName);
+      process.stdout.write(`|>|>|>|> unlinkin ${pFileName} done\n`);
+    } catch (e) {
+      // probably files didn't exist in the first place
+      // so ignore the exception
+      process.stdout.write(`|>|>|>|> could not unlink ${pFileName} - ${e}\n`);
+    } finally {
+      // explicitly ignore finally as well
+      process.stdout.write(`|>|>|>|> finally!\n`);
+    }
+  }
+
   before("set up", () => {
     process.stdout.write(`|>|>|> set up - started\n`);
     fs.writeFileSync(OUTFILE, "{}", "utf8");
