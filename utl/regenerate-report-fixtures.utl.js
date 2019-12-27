47d58c6d89af872fa26bb176d39763ad86f43c2b
Sander Verweij <sverweij@users.noreply.github.com>
2019-12-27 11:54:01 +0100

    refactor(test): unify the reporter test mocks & fixtures (#230)

---
+++ utl/regenerate-report-fixtures.utl.js
@@ -0,0 +1,67 @@
+const fs = require("fs");
+const path = require("path");
+const renderDdot = require("../src/report/dot/folderLevel");
+const renderDot = require("../src/report/dot/moduleLevel");
+const renderTeamcity = require("../src/report/teamcity");
+const renderHTML = require("../src/report/html");
+const renderCSV = require("../src/report/csv");
+
+function thingify(pInputFileName, pOutputFileName, pFunction) {
+  fs.writeFileSync(
+    pOutputFileName,
+    pFunction(JSON.parse(fs.readFileSync(pInputFileName, "utf8"))).output,
+    "utf8"
+  );
+}
+
+function regenerateFixtures(pDir, pFunction, pTargetExtension) {
+  fs.readdirSync(pDir)
+    .filter(pFileName => pFileName.endsWith(".json"))
+    .map(pFileName => ({
+      inputFileName: path.join(pDir, pFileName),
+      outputFileName: path.join(
+        pDir,
+        pFileName.replace(/\.json$/g, pTargetExtension)
+      )
+    }))
+    .forEach(pPair => {
+      thingify(pPair.inputFileName, pPair.outputFileName, pFunction);
+    });
+}
+
+function renderBareThemeDot(pResultObject) {
+  const lBareTheme = JSON.parse(
+    fs.readFileSync(
+      path.join(__dirname, "../test/report/dot/moduleLevel/bareTheme.json")
+    )
+  );
+
+  return renderDot(pResultObject, lBareTheme);
+}
+
+function renderDefaultThemeDot(pResultObject) {
+  // empty theme with no overrides gets the default theme assigned
+  return renderDot(pResultObject, {});
+}
+
+const DDOT_MOCK_DIR = path.join(
+  __dirname,
+  "../test/report/dot/folderLevel/mocks/"
+);
+const DOT_MOCK_DIR = path.join(
+  __dirname,
+  "../test/report/dot/moduleLevel/mocks/"
+);
+const TEAMCITY_MOCK_DIR = path.join(
+  __dirname,
+  "../test/report/teamcity/mocks/"
+);
+const HTML_MOCK_DIR = path.join(__dirname, "../test/report/html/mocks/");
+const CSV_MOCK_DIR = path.join(__dirname, "../test/report/csv/mocks/");
+
+regenerateFixtures(DDOT_MOCK_DIR, renderDdot, ".dot");
+regenerateFixtures(DOT_MOCK_DIR, renderBareThemeDot, ".dot");
+regenerateFixtures(DOT_MOCK_DIR, renderDefaultThemeDot, "-default-theme.dot");
+regenerateFixtures(TEAMCITY_MOCK_DIR, renderTeamcity, "-teamcity-format.txt");
+regenerateFixtures(HTML_MOCK_DIR, renderHTML, ".html");
+regenerateFixtures(CSV_MOCK_DIR, renderCSV, ".csv");
