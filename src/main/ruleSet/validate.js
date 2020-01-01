1ee7a1c3ef50d63054f03e57ecf2abe1183761c6
Sander Verweij <sverweij@users.noreply.github.com>
2020-01-01 21:59:19 +0100

    refactor(main): check regex safety with external version of safe-regexp again (#234)

--- src/main/ruleSet/validate.js
+++ src/main/ruleSet/validate.js
@@ -1,5 +1,5 @@
 const Ajv = require("ajv");
-const safeRegex = require("../../utl/safe-regex");
+const safeRegex = require("safe-regex");
 const validateOptions = require("../options/validate");
 const configurationSchema = require("../../schema/configuration.schema.json");
 
