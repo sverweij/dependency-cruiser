7235732e1f4c6408ecd9b60c90b4a6fcc1dc1193
sverweij <sverweij@users.noreply.github.com>
2020-02-09 10:13:15 +0100

    refactor(report): isolate consolidation functions for use outside dot reporters

---
+++ src/report/utl/consolidateToPattern.js
@@ -0,0 +1,34 @@
+const consolidateModules = require("./consolidateModules");
+const consolidateModuleDependencies = require("./consolidateModuleDependencies");
+
+function squashDependencyToPattern(pCollapsePattern) {
+  return pDependency => {
+    const lCollapseMatch = pDependency.resolved.match(pCollapsePattern);
+
+    return {
+      ...pDependency,
+      resolved: lCollapseMatch ? lCollapseMatch[0] : pDependency.resolved
+    };
+  };
+}
+function squashModuleToPattern(pCollapsePattern) {
+  return pModule => {
+    const lCollapseMatch = pModule.source.match(pCollapsePattern);
+
+    return {
+      ...pModule,
+      source: lCollapseMatch ? lCollapseMatch[0] : pModule.source,
+      consolidated: lCollapseMatch
+        ? lCollapseMatch[0] !== pModule.source
+        : false,
+      dependencies: pModule.dependencies.map(
+        squashDependencyToPattern(pCollapsePattern)
+      )
+    };
+  };
+}
+
+module.exports = (pModules, pCollapsePattern) =>
+  consolidateModules(pModules.map(squashModuleToPattern(pCollapsePattern))).map(
+    consolidateModuleDependencies
+  );
