ac0b714abaa2b4f45d43c8cf8abcbddd25c82399
sverweij <sverweij@users.noreply.github.com>
2020-05-06 18:30:28 +0200

    test(cli): adds a unit test for exit code

--- test/cli/format.spec.js
+++ test/cli/format.spec.js
@@ -10,13 +10,37 @@ describe("cli/format", () => {
 
     deleteDammit(lOutFile);
 
-    await format(path.join(__dirname, "fixtures", "empty.json"), {
-      outputTo: lOutFile,
-    });
+    const lExitCode = await format(
+      path.join(__dirname, "fixtures", "empty.json"),
+      {
+        outputTo: lOutFile,
+      }
+    );
 
     expect(fs.readFileSync(lOutFile, "utf8")).to.contain(
       "dependencies cruised"
     );
+    expect(lExitCode).to.equal(0);
+    deleteDammit(lOutFile);
+  });
+
+  it("returns a non-zero exit code when there's error level dependency violations in the output and ", async () => {
+    const lOutFile = "otherthing";
+
+    deleteDammit(lOutFile);
+
+    const lExitCode = await format(
+      path.join(
+        __dirname,
+        "fixtures",
+        "result-has-a-dependency-violation.json"
+      ),
+      {
+        outputTo: lOutFile,
+      }
+    );
+
+    expect(lExitCode).to.equal(1);
     deleteDammit(lOutFile);
   });
 });
