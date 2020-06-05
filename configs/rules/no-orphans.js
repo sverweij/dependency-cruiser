29f6d7a974b22018c85abc7c118ca3e59db4e6cb
Sander Verweij <sverweij@users.noreply.github.com>
2020-06-05 16:16:59 +0200

    feature(init|configs): exclude some known config files from the default orphan rule scrutiny (#311)

--- configs/rules/no-orphans.js
+++ configs/rules/no-orphans.js
@@ -1,13 +1,28 @@
+const DOT_FILE_PATTERN = "(^|/)\\.[^/]+\\.(js|cjs|mjs|ts|json)$";
+const TS_DECLARATION_FILE_PATTERN = "\\.d\\.ts$";
+const TS_CONFIG_FILE_PATTERN = "(^|/)tsconfig\\.json$";
+const OTHER_CONFIG_FILES_PATTERN =
+  "(^|/)(babel|webpack)\\.config\\.(js|cjs|mjs|ts|json)$";
+
+const KNOWN_CONFIG_FILE_PATTERNS = [
+  DOT_FILE_PATTERN,
+  TS_DECLARATION_FILE_PATTERN,
+  TS_CONFIG_FILE_PATTERN,
+  OTHER_CONFIG_FILES_PATTERN,
+].join("|");
+
 module.exports = {
   name: "no-orphans",
   comment:
     "This is an orphan module - it's likely not used (anymore?). Either use it or " +
     "remove it. If it's logical this module is an orphan (i.e. it's a config file), " +
-    "add an exception for it in your dependency-cruiser configuration.",
+    "add an exception for it in your dependency-cruiser configuration. By default " +
+    "this rule does not scrutinize dotfiles (e.g. .eslintrc.js), TypeScript declaration " +
+    "files (.d.ts), tsconfig.json and some of the babel and webpack configs.",
   severity: "warn",
   from: {
     orphan: true,
-    pathNot: "\\.d\\.ts$",
+    pathNot: KNOWN_CONFIG_FILE_PATTERNS,
   },
   to: {},
 };
---
+++ test/configs/no-orphans.spec.js
@@ -0,0 +1,85 @@
+const chai = require("chai");
+const noOrphansRule = require("../../configs/rules/no-orphans");
+const matchModuleRule = require("../../src/validate/match-module-rule");
+
+const expect = chai.expect;
+
+describe("configs/rules/no-orphans", () => {
+  it("flags non-excepted orphans as orphan rule transgression", () => {
+    expect(
+      matchModuleRule.matchesOrphanRule(noOrphansRule, {
+        source: "Rémi.js",
+        orphan: true,
+      })
+    ).to.deep.equal(true);
+  });
+
+  it("flags files ending on a dotfile as orphan rule transgression", () => {
+    expect(
+      matchModuleRule.matchesOrphanRule(noOrphansRule, {
+        source: "looks-like-a-dot-sorta.Rémi.js",
+        orphan: true,
+      })
+    ).to.deep.equal(true);
+  });
+
+  it("does not flag dot files not as orphan rule transgressions", () => {
+    expect(
+      matchModuleRule.matchesOrphanRule(noOrphansRule, {
+        source: ".Rémi.js",
+        orphan: true,
+      })
+    ).to.deep.equal(false);
+  });
+
+  it("does not flag dot files in the tree as orphan rule transgressions", () => {
+    expect(
+      matchModuleRule.matchesOrphanRule(noOrphansRule, {
+        source: "packages/thing/.Rémi.js",
+        orphan: true,
+      })
+    ).to.deep.equal(false);
+  });
+
+  it("does not flag dot files in the tree as orphan rule transgressions, regardless extension", () => {
+    expect(
+      matchModuleRule.matchesOrphanRule(noOrphansRule, {
+        source: "packages/thing/.Rémi.ts",
+        orphan: true,
+      })
+    ).to.deep.equal(false);
+  });
+
+  it("does not flag any .d.ts not as orphan rule transgressions", () => {
+    expect(
+      matchModuleRule.matchesOrphanRule(noOrphansRule, {
+        source: "packages/thing/types/lalalal.d.ts",
+        orphan: true,
+      })
+    ).to.deep.equal(false);
+    expect(
+      matchModuleRule.matchesOrphanRule(noOrphansRule, {
+        source: "lalalal.d.ts",
+        orphan: true,
+      })
+    ).to.deep.equal(false);
+  });
+
+  it("does not flag babel config files in the tree not as orphan rule transgressions", () => {
+    expect(
+      matchModuleRule.matchesOrphanRule(noOrphansRule, {
+        source: "packages/thing/babel.config.mjs",
+        orphan: true,
+      })
+    ).to.deep.equal(false);
+  });
+
+  it("does not flag babel config files as orphan rule transgressions", () => {
+    expect(
+      matchModuleRule.matchesOrphanRule(noOrphansRule, {
+        source: "babel.config.mjs",
+        orphan: true,
+      })
+    ).to.deep.equal(false);
+  });
+});
