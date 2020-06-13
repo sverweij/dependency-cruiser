d8b90fbcf7cfe6e0ebc147993bb0b56b1fd0e039
Sander Verweij <sverweij@users.noreply.github.com>
2020-06-13 14:20:00 +0200

    feature(cli): enables reading babel configs from package.json (#320)

--- src/cli/parse-babel-config.js
+++ src/cli/parse-babel-config.js
@@ -1,7 +1,22 @@
+/* eslint-disable security/detect-non-literal-require */
+/* eslint-disable import/no-dynamic-require */
+/* eslint-disable node/global-require */
 const fs = require("fs");
 const json5 = require("json5");
+const _get = require("lodash/get");
 const tryRequire = require("semver-try-require");
 const $package = require("../../package.json");
+
+function getConfig(pBabelConfigFileName) {
+  let lReturnValue = {};
+
+  lReturnValue = json5.parse(fs.readFileSync(pBabelConfigFileName, "utf8"));
+
+  if (pBabelConfigFileName.endsWith("package.json")) {
+    lReturnValue = _get(lReturnValue, "babel", {});
+  }
+  return lReturnValue;
+}
 
 module.exports = function parseBabelConfig(pBabelConfigFileName) {
   let lReturnValue = {};
@@ -9,9 +24,7 @@ module.exports = function parseBabelConfig(pBabelConfigFileName) {
 
   /* istanbul ignore else */
   if (babel) {
-    lReturnValue = babel.loadOptions(
-      json5.parse(fs.readFileSync(pBabelConfigFileName, "utf8"))
-    );
+    lReturnValue = babel.loadOptions(getConfig(pBabelConfigFileName));
   }
 
   return lReturnValue;
