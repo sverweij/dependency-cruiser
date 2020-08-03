08e0925e06a5e8ecaa79b3c1c5d9fe3b7cda54b4
sverweij <sverweij@users.noreply.github.com>
2020-08-03 20:06:53 +0200

    doc(validate): improve jsdoc

--- src/validate/utl.js
+++ src/validate/utl.js
@@ -1,15 +1,21 @@
-/* if there is at least one group expression in the given pRulePath
-   return the first matched one.
-   return null in all other cases
-
-   This fills our current need. Later we can expand it to return all group
-   matches.
-*/
-function extractGroups(pRule, pActualPath) {
+/**
+ * If there is at least one group expression in the given pRulePath
+ * return the first matched one.
+ *
+ * Return null in all other cases.
+ *
+ * This fills our current need. Later we can expand it to return all group
+ * matches.
+ *
+ * @param {import("../../types/rule-set").IFromRestriction} pRestriction
+ * @param {string} pActualPath
+ * @returns {string[]|null}
+ */
+function extractGroups(pRestriction, pActualPath) {
   let lReturnValue = [];
 
-  if (Boolean(pRule.path)) {
-    let lMatchResult = pActualPath.match(pRule.path);
+  if (Boolean(pRestriction.path)) {
+    let lMatchResult = pActualPath.match(pRestriction.path);
 
     if (Boolean(lMatchResult) && lMatchResult.length > 1) {
       lReturnValue = lMatchResult.filter(
--- src/validate/violates-required-rule.js
+++ src/validate/violates-required-rule.js
@@ -1,6 +1,14 @@
 const matchers = require("./matchers");
 const { extractGroups } = require("./utl");
 
+/**
+ * Returns true if the module violates the rule.
+ * Returns false in all other cases.
+ *
+ * @param {import("../../types/rule-set").IRequiredRuleType} pRule
+ * @param {import("../../types/cruise-result").IModule} pModule
+ * @returns {boolean}
+ */
 module.exports = function violatesRequiredRule(pRule, pModule) {
   let lReturnValue = false;
 
