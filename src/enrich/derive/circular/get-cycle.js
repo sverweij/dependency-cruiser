689ec5c304b80a9d677888ad3113a18ae037f96a
sverweij <sverweij@users.noreply.github.com>
2020-08-10 22:09:28 +0200

    resurect accidentally removed comments

--- src/enrich/derive/circular/get-cycle.js
+++ src/enrich/derive/circular/get-cycle.js
@@ -1,5 +1,27 @@
 const findNodeByName = require("../find-module-by-name");
+/* about the absence of checks whether attributes/ objects actually
+ * exist:
+ * - it saves CPU cycles to the effect of being ~30% faster than with the
+ *   checks
+ * - .dependencies is a mandatory attribute (as per json schema)
+ * - .resolved is a mandatory attribute (as per json schema)
+ */
 
+/**
+ * Returns the first non-zero length path from pInitialSource to pInitialSource
+ * Returns the empty array if there is no such path
+ *
+ * @param  {any} pGraph      The graph in which to test this condition
+ * @param  {string} pInitialSource The 'source' attribute of the node to be tested
+ *                              (source uniquely identifying a node)
+ * @param  {string} pCurrentSource   The 'source' attribute of the 'to' node to
+ *                              be traversed
+ * @param  {Set} pVisited       The set of nodes visited hithereto (optional
+ *                              attribute - there's no need to pass it when
+ *                              calling it; it's used by the algorithm to
+ *                              determine the stop condition)
+ * @return {string[]}           see description above
+ */
 function getCycle(pGraph, pInitialSource, pCurrentSource, pVisited) {
   pVisited = pVisited || new Set();
 
