cae5a0722f3c9be796777ab275dad94e34cb1cd6
sverweij <sverweij@users.noreply.github.com>
2020-12-28 14:45:09 +0100

    bugfix(babel): gives babel plugins a filename to work with

--- src/extract/transpile/babel-wrap.js
+++ src/extract/transpile/babel-wrap.js
@@ -6,5 +6,10 @@ const babel = tryRequire("@babel/core", $package.supportedTranspilers.babel);
 module.exports = {
   isAvailable: () => babel !== false,
   transpile: (pSource, pTranspileOptions = {}) =>
-    babel.transformSync(pSource, pTranspileOptions.babelConfig || {}).code,
+    babel.transformSync(pSource, {
+      ...(pTranspileOptions.babelConfig || {}),
+      // Some babel plugins assume a piece of source to have a filename.
+      // See https://github.com/sverweij/dependency-cruiser/issues/410
+      filename: "dummy-filename.js",
+    }).code,
 };
