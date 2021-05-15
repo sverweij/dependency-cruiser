c8a3d4d948ed9d4bd3ba74fafd26f1e740acfe5e
Sander Verweij <sverweij@users.noreply.github.com>
2021-05-15 16:32:15 +0200

    refactor(swc): consolidate swc call (#466)

--- src/extract/ast-extractors/extract-swc-deps.js
+++ src/extract/ast-extractors/extract-swc-deps.js
@@ -2,17 +2,6 @@ const SwcDependencyVisitor = require("./swc-dependency-visitor");
 
 /**
  *
- * @param {import('@swc/core').ModuleItem[]} pAST
- * @param {string[]} pExoticRequireStrings
- * @returns {{module: string, moduleSystem: string, dynamic: boolean}[]}
- */
-function extractNestedDependencies(pAST, pExoticRequireStrings) {
-  const visitor = new SwcDependencyVisitor(pExoticRequireStrings);
-  return visitor.getDependencies(pAST);
-}
-
-/**
- *
  * @param {import('@swc/core').ModuleItem[]} pSwcAST
  * @param {string[]} pExoticRequireStrings
  * @returns {{module: string, moduleSystem: string, dynamic: boolean}[]}
@@ -21,10 +10,8 @@ module.exports = function extractSwcDependencies(
   pSwcAST,
   pExoticRequireStrings
 ) {
-  return (
-    extractNestedDependencies(pSwcAST, pExoticRequireStrings)
-      // as far as I can tell swc doesn't do tripple slash directives
-      // .concat(extractTrippleSlashDirectives(pSwcAST))
-      .map((pModule) => ({ dynamic: false, ...pModule }))
-  );
+  const visitor = new SwcDependencyVisitor(pExoticRequireStrings);
+  return visitor
+    .getDependencies(pSwcAST)
+    .map((pModule) => ({ dynamic: false, ...pModule }));
 };
