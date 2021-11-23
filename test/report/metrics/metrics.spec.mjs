37921ed8e937d3f59c7ee9545a3cb18bb16fc981
sverweij <sverweij@users.noreply.github.com>
2021-11-23 21:24:57 +0100

    feature: adds options to the metrics reporter
    order by name, moduleCount, efferentCouplings, afferentCouplings or Instability; leave out modules or folders (or both ¯\_ (ツ)_/¯ )

--- test/report/metrics/metrics.spec.mjs
+++ test/report/metrics/metrics.spec.mjs
@@ -28,7 +28,28 @@ describe("report/metrics", () => {
     expect(lResult.output).to.contain("src      1     1     1  0.5");
   });
 
-  it("emits module metrics (sorted)", () => {
+  it("does not emit folder metrics when asked to hide them", () => {
+    const lResult = metrics(
+      {
+        modules: [],
+        folders: [
+          {
+            name: "src",
+            moduleCount: 1,
+            afferentCouplings: 1,
+            efferentCouplings: 1,
+            instability: 0.5,
+          },
+        ],
+      },
+      { hideFolders: true }
+    );
+
+    expect(lResult.exitCode).to.equal(0);
+    expect(lResult.output).to.not.contain("src      1     1     1  0.5");
+  });
+
+  it("emits module metrics (sorted by instability by default)", () => {
     const lResult = metrics({
       modules: [
         {
@@ -55,6 +76,74 @@ describe("report/metrics", () => {
 
     expect(lResult.exitCode).to.equal(0);
     expect(lResult.output).to.contain(
+      `src/mies.js     1     1     1  0.5 ${EOL}src/aap.js      1     1     3  0.25${EOL}src/noot`
+    );
+  });
+
+  it("emits module metrics (sorted by name)", () => {
+    const lResult = metrics(
+      {
+        modules: [
+          {
+            source: "src/noot.js",
+            dependencies: [],
+            dependents: ["two"],
+            instability: 0,
+          },
+          {
+            source: "src/aap.js",
+            dependencies: ["one", "two", "three"],
+            dependents: ["four"],
+            instability: 0.25,
+          },
+          {
+            source: "src/mies.js",
+            dependencies: ["one"],
+            dependents: ["two"],
+            instability: 0.5,
+          },
+        ],
+        folders: [],
+      },
+      { orderBy: "name" }
+    );
+
+    expect(lResult.exitCode).to.equal(0);
+    expect(lResult.output).to.contain(
+      `src/aap.js      1     1     3  0.25${EOL}src/mies.js     1     1     1  0.5 ${EOL}src/noot.js`
+    );
+  });
+
+  it("emits no module metrics when asked to hide them", () => {
+    const lResult = metrics(
+      {
+        modules: [
+          {
+            source: "src/noot.js",
+            dependencies: [],
+            dependents: ["two"],
+            instability: 0,
+          },
+          {
+            source: "src/aap.js",
+            dependencies: ["one", "two", "three"],
+            dependents: ["four"],
+            instability: 0.25,
+          },
+          {
+            source: "src/mies.js",
+            dependencies: ["one"],
+            dependents: ["two"],
+            instability: 0.5,
+          },
+        ],
+        folders: [],
+      },
+      { hideModules: true }
+    );
+
+    expect(lResult.exitCode).to.equal(0);
+    expect(lResult.output).to.not.contain(
       `src/mies.js     1     1     1  0.5 ${EOL}src/aap.js      1     1     3  0.25${EOL}src/noot`
     );
   });
