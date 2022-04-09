3c83b7a5020bc3ed8a0b3d754c239ba0e3b93895
Sander Verweij <sverweij@users.noreply.github.com>
2022-04-09 21:28:22 +0200

    feature(validate): adds possibility to validate folder cycles (#579)

--- test/validate/match-folder-rule.spec.mjs
+++ test/validate/match-folder-rule.spec.mjs
@@ -3,11 +3,15 @@ import matchFolderRule from "../../src/validate/match-folder-dependency-rule.js"
 
 const EMPTY_RULE = { scope: "folder", from: {}, to: {} };
 const SDP_RULE = { scope: "folder", from: {}, to: { moreUnstable: true } };
+const CYCLE_RULE = { scope: "folder", from: {}, to: { circular: true } };
 
-describe("[I] validate/match-folder-dependency-rule - match", () => {
+describe("[I] validate/match-folder-dependency-rule - match generic", () => {
   it("empty rule => match all the things (empty from & to)", () => {
     expect(matchFolderRule.match({}, {})(EMPTY_RULE)).to.equal(true);
   });
+});
+
+describe("[I] validate/match-folder-dependency-rule - match SDP", () => {
   it("rule with a restriction on the to doesn't match when the criterium is not met (data missing)", () => {
     expect(matchFolderRule.match({}, {})(SDP_RULE)).to.equal(false);
   });
@@ -20,5 +24,21 @@ describe("[I] validate/match-folder-dependency-rule - match", () => {
     expect(
       matchFolderRule.match({ instability: 0 }, { instability: 1 })(SDP_RULE)
     ).to.equal(true);
+  });
+});
+
+describe("[I] validate/match-folder-dependency-rule - match cycle", () => {
+  it("rule with a restriction on the to doesn't match when the criterium is not met (data missing)", () => {
+    expect(matchFolderRule.match({}, {})(CYCLE_RULE)).to.equal(false);
+  });
+  it("rule with a restriction on the to doesn't match when the criterium is not met (data there)", () => {
+    expect(matchFolderRule.match({}, { circular: false })(CYCLE_RULE)).to.equal(
+      false
+    );
+  });
+  it("rule with a restriction on the to doesn't match when the criterium is met (data there)", () => {
+    expect(matchFolderRule.match({}, { circular: true })(CYCLE_RULE)).to.equal(
+      true
+    );
   });
 });
