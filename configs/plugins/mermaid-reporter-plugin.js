dcf12a45e69ff8d09444cf688bf71b7f576b6e7a
Hirotaka Miyagi <31152321+MH4GF@users.noreply.github.com>
2022-05-29 18:32:21 +0900

    feature(plugin): adds mermaid.js reporter plugin (#599)
    Co-authored-by: sverweij <sverweij@users.noreply.github.com>

---
+++ configs/plugins/mermaid-reporter-plugin.js
@@ -0,0 +1,111 @@
+const ACORN_DUMMY_VALUE = "✖";
+
+/* eslint-disable security/detect-object-injection */
+const mermaidNode = (pNode, pText) => {
+  const lNode = pNode
+    .replace(ACORN_DUMMY_VALUE, "__unknown__")
+    .replace(/^\.$|^\.\//g, "__currentPath__")
+    .replace(/^\.{2}$|^\.{2}\//g, "__prevPath__")
+    .replace(/[[\]/.@]/g, "_");
+  const lText = pText ? `["${pText}"]` : "";
+  return `${lNode}${lText}`;
+};
+
+const mermaidEdge = (pFrom, pTo) => {
+  const lFromNode = mermaidNode(pFrom.node);
+  const lToNode = mermaidNode(pTo.node);
+  return `${lFromNode} --> ${lToNode}`;
+};
+
+const mermaidEdges = (pEdges) => {
+  return pEdges.map((pEdge) => mermaidEdge(pEdge.from, pEdge.to)).join("\n");
+};
+
+const convertedEdgeSources = (pCruiseResult) => {
+  return pCruiseResult.modules.flatMap((pModule) => {
+    const lFrom = {
+      node: pModule.source,
+      text: pModule.source.split("/").slice(-1)[0],
+    };
+
+    return pModule.dependencies.map((pDep) => {
+      return {
+        from: lFrom,
+        to: {
+          node: pDep.resolved,
+          text: pDep.resolved.split("/").slice(-1)[0],
+        },
+      };
+    });
+  });
+};
+
+const indent = (pDepth = 0) => {
+  return "  ".repeat(pDepth);
+};
+
+const mermaidSubgraph = (pNode, pText, pChildren, pDepth) => {
+  return `${indent(pDepth)}subgraph ${mermaidNode(pNode, pText)}
+${pChildren}
+${indent(pDepth)}end`;
+};
+
+const mermaidSubgraphs = (pSource, pDepth = 0) => {
+  return Object.keys(pSource)
+    .map((pName) => {
+      const source = pSource[pName];
+      const children = mermaidSubgraphs(source.children, pDepth + 1);
+      if (children === "")
+        return `${indent(pDepth)}${mermaidNode(source.node, source.text)}`;
+
+      return mermaidSubgraph(source.node, source.text, children, pDepth);
+    })
+    .join("\n");
+};
+
+const convertedSubgraphSources = (pCruiseResult) => {
+  let lTree = {};
+
+  pCruiseResult.modules.forEach((pModule) => {
+    const paths = pModule.source.split("/");
+
+    paths.reduce((pChildren, pCurrentPath, pIndex) => {
+      if (!pChildren[pCurrentPath]) {
+        pChildren[pCurrentPath] = {
+          node: paths.slice(0, pIndex + 1).join("/"),
+          text: pCurrentPath,
+          children: {},
+        };
+      }
+      return pChildren[pCurrentPath].children;
+    }, lTree);
+  });
+
+  return lTree;
+};
+
+const renderMermaidThing = (pCruiseResult) => {
+  const subgraphs = convertedSubgraphSources(pCruiseResult);
+  const edges = convertedEdgeSources(pCruiseResult);
+
+  return `flowchart LR
+
+${mermaidSubgraphs(subgraphs)}
+${mermaidEdges(edges)}
+`;
+};
+
+/**
+ * mermaid reporter plugin
+ *
+ * @param {import('../../types/dependency-cruiser').ICruiseResult} pCruiseResult -
+ *      the output of a dependency-cruise adhering to dependency-cruiser's
+ *      cruise result schema
+ * @return {import('../../types/dependency-cruiser').IReporterOutput} -
+ *      output: a string
+ *      exitCode: 0
+ */
+module.exports = (pCruiseResult) => ({
+  output: renderMermaidThing(pCruiseResult),
+  exitCode: 0,
+});
---
+++ test/configs/plugins/mermaid-reporter-plugin/module-level/index.spec.mjs
@@ -0,0 +1,33 @@
+import { readFileSync } from "node:fs";
+import { join } from "node:path";
+import { fileURLToPath } from "node:url";
+import { expect } from "chai";
+import mermaidReporterPlugin from "../../../../../configs/plugins/mermaid-reporter-plugin.js";
+import { createRequireJSON } from "../../../../backwards.utl.mjs";
+
+const requireJSON = createRequireJSON(import.meta.url);
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
+const mockPath = join(__dirname, "__mocks__");
+
+const same = (pName) => {
+  const definition = requireJSON(`./__mocks__/${pName}.json`);
+  const expected = readFileSync(join(mockPath, `${pName}.mmd`), "utf8");
+  const output = mermaidReporterPlugin(definition).output;
+  expect(output).to.deep.equal(expected);
+};
+
+describe("[I] configs/plugins/mermaid-reporter-plugin module-level reporter", () => {
+  it("renders a mermaid - render directories", () =>
+    same("dependency-cruiser-2019-01-14"));
+  it("renders a mermaid - modules in the root don't come in a cluster", () =>
+    same("clusterless"));
+  it("renders a mermaid - unresolvable in a sub folder (either existing or not) get labeled as unresolvable", () =>
+    same("es6-unresolvable-deps"));
+  it("renders a mermaid - matchesDoNotFollow NOT rendered as folders", () =>
+    same("do-not-follow-deps"));
+  it("renders a mermaid - renders orphan module", () => same("orphan-deps"));
+  it("renders a mermaid - rendered strings that mermaid cannot parse are escaped.", () =>
+    same("contains-strings-to-be-escaped"));
+  it("renders a mermaid - rendered strings that replaced from unknown figures.", () =>
+    same("unknown-deps"));
+});
