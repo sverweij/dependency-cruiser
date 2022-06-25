2f9728bef29d6171b8f9d3ccc3d4029e5324da92
sverweij <sverweij@users.noreply.github.com>
2022-06-25 13:33:14 +0200

    refactor so both name hashing functions occur on the same spot
    this way the render functions only have to deal with one interface (get the hashed name from the hashmap)

--- src/report/mermaid.js
+++ src/report/mermaid.js
@@ -12,11 +12,11 @@ const hashNodeName = (pNode) =>
     .replace(/^\.{2}$|^\.{2}\//g, "__prevPath__")
     .replace(/[[\]/.@~-]/g, "_");
 
-const mermaidNode = (pNode, pText) => `${hashNodeName(pNode)}["${pText}"]`;
+const mermaidNode = (pNode, pText) => `${pNode}["${pText}"]`;
 
 const mermaidEdge = (pFrom, pTo) => {
-  const lFromNode = hashNodeName(pFrom.node);
-  const lToNode = hashNodeName(pTo.node);
+  const lFromNode = pFrom.node;
+  const lToNode = pTo.node;
   return `${lFromNode}-->${lToNode}`;
 };
 
@@ -26,18 +26,18 @@ const mermaidEdges = (pEdges) => {
 
 /**
  * @param {import('../../types/dependency-cruiser').ICruiseResult} pCruiseResult
- * @param {Map<string, string>} pMinifiedNames
+ * @param {Map<string, string>} pNamesHashMap
  */
-const convertedEdgeSources = (pCruiseResult, pMinifiedNames) => {
+const convertedEdgeSources = (pCruiseResult, pNamesHashMap) => {
   return pCruiseResult.modules.flatMap((pModule) => {
-    const lFromNode = pMinifiedNames.get(pModule.source) || pModule.source;
+    const lFromNode = pNamesHashMap.get(pModule.source);
     const lFrom = {
       node: lFromNode,
       text: pModule.source.split("/").slice(-1)[0],
     };
 
     return pModule.dependencies.map((pDep) => {
-      const lToNode = pMinifiedNames.get(pDep.resolved) || pDep.resolved;
+      const lToNode = pNamesHashMap.get(pDep.resolved);
       return {
         from: lFrom,
         to: {
@@ -49,8 +49,8 @@ const convertedEdgeSources = (pCruiseResult, pMinifiedNames) => {
   });
 };
 
-const indent = (pDepth = 0) => {
-  return "  ".repeat(pDepth);
+const indent = (pDepth = 0, pMinify) => {
+  return pMinify ? "" : "  ".repeat(pDepth);
 };
 
 const mermaidSubgraph = (pNode, pText, pChildren, pIndent) => {
@@ -62,7 +62,7 @@ ${pIndent}end`;
 const mermaidSubgraphs = (pSource, pOptions, pDepth = 0) => {
   return Object.keys(pSource)
     .map((pName) => {
-      const lIndent = pOptions.minify ? "" : indent(pDepth);
+      const lIndent = indent(pDepth, pOptions.minify);
       const source = pSource[pName];
       const children = mermaidSubgraphs(source.children, pOptions, pDepth + 1);
       if (children === "")
@@ -87,7 +87,7 @@ const convertedSubgraphSources = (pCruiseResult, pMinifiedNames) => {
       if (!pChildren[pCurrentPath]) {
         const node = paths.slice(0, pIndex + 1).join("/");
         pChildren[pCurrentPath] = {
-          node: pMinifiedNames.get(node) || node,
+          node: pMinifiedNames.get(node),
           text: pCurrentPath,
           children: {},
         };
@@ -101,16 +101,16 @@ const convertedSubgraphSources = (pCruiseResult, pMinifiedNames) => {
 
 /**
  * @param {import("../../types/cruise-result").IModule[]} pModules
- * @param {Map<string, string>} pMinifiedNames
+ * @param {Map<string, string>} pNamesHashMap
  */
-const focusHighlights = (pModules, pMinifiedNames) => {
+const focusHighlights = (pModules, pNamesHashMap) => {
   const lHighLightStyle = "fill:lime,color:black";
 
   return pModules
     .filter((pModule) => pModule.matchesFocus === true)
     .reduce((pAll, pModule) => {
-      const lSource = pMinifiedNames.get(pModule.source) || pModule.source;
-      return (pAll += `\nstyle ${hashNodeName(lSource)} ${lHighLightStyle}`);
+      const lSource = pNamesHashMap.get(pModule.source);
+      return (pAll += `\nstyle ${lSource} ${lHighLightStyle}`);
     }, "");
 };
 
@@ -119,24 +119,28 @@ const focusHighlights = (pModules, pMinifiedNames) => {
  * @param {Boolean} pMinify
  * @return {Map<string, string>}
  */
-const minifiedNames = (pModules, pMinify) => {
-  const names = new Map();
-  if (!pMinify) return names;
-
+const hashModuleNames = (pModules, pMinify) => {
+  const lBase = 10;
+  const lNamesHashMap = new Map();
   let lCount = 0;
+
   pModules.forEach((pModule) => {
     const lPaths = pModule.source.split("/");
 
     for (let lIndex = 0; lIndex < lPaths.length; lIndex += 1) {
       const lName = lPaths.slice(0, lIndex + 1).join("/");
-      if (!names.get(lName)) {
-        names.set(lName, lCount.toString());
-        lCount += 1;
+      if (!lNamesHashMap.get(lName)) {
+        if (pMinify) {
+          lNamesHashMap.set(lName, lCount.toString(lBase));
+          lCount += 1;
+        } else {
+          lNamesHashMap.set(lName, hashNodeName(lName));
+        }
       }
     }
   });
 
-  return names;
+  return lNamesHashMap;
 };
 
 /**
@@ -145,15 +149,15 @@ const minifiedNames = (pModules, pMinify) => {
  */
 const renderMermaidThing = (pCruiseResult, pOptions) => {
   const lOptions = { ...REPORT_DEFAULTS, ...(pOptions || {}) };
-  const names = minifiedNames(pCruiseResult.modules, lOptions.minify);
-  const subgraphs = convertedSubgraphSources(pCruiseResult, names);
-  const edges = convertedEdgeSources(pCruiseResult, names);
+  const lNamesHashMap = hashModuleNames(pCruiseResult.modules, lOptions.minify);
+  const lSubgraphs = convertedSubgraphSources(pCruiseResult, lNamesHashMap);
+  const lEdges = convertedEdgeSources(pCruiseResult, lNamesHashMap);
 
   return `flowchart LR
 
-${mermaidSubgraphs(subgraphs, lOptions)}
-${mermaidEdges(edges)}
-${focusHighlights(pCruiseResult.modules, names)}`;
+${mermaidSubgraphs(lSubgraphs, lOptions)}
+${mermaidEdges(lEdges)}
+${focusHighlights(pCruiseResult.modules, lNamesHashMap)}`;
 };
 
 /**
