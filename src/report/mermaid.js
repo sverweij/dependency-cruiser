cd91eb09a00a85aa093303bc12b3688126839b62
Sander Verweij <sverweij@users.noreply.github.com>
2022-06-26 13:17:54 +0200

    bugfix(mermaid): small fixes to types, tests (#626)
    * bugfix(types): make mermaid optional in options type
    * bugfix(test|mermaid): adds scenario where no options are passed to the reporter
    * refactor(mermaid): use hashmap.has instead of hashmap.get to test whether a key is in
    * bugfix(test): remove stray console.dir left in 05febad
    * style(mermaid): apply some code style rules that don't have an automated check on them yet

--- src/report/mermaid.js
+++ src/report/mermaid.js
@@ -1,34 +1,21 @@
 /* eslint-disable security/detect-object-injection */
 const ACORN_DUMMY_VALUE = "✖";
-
 const REPORT_DEFAULTS = {
   minify: true,
 };
 
-const hashNodeName = (pNode) =>
-  pNode
-    .replace(ACORN_DUMMY_VALUE, "__unknown__")
-    .replace(/^\.$|^\.\//g, "__currentPath__")
-    .replace(/^\.{2}$|^\.{2}\//g, "__prevPath__")
-    .replace(/[[\]/.@~-]/g, "_");
+const renderNode = (pNode, pText) => `${pNode}["${pText}"]`;
 
-const mermaidNode = (pNode, pText) => `${pNode}["${pText}"]`;
+const renderEdge = (pFrom, pTo) => `${pFrom.node}-->${pTo.node}`;
 
-const mermaidEdge = (pFrom, pTo) => {
-  const lFromNode = pFrom.node;
-  const lToNode = pTo.node;
-  return `${lFromNode}-->${lToNode}`;
-};
-
-const mermaidEdges = (pEdges) => {
-  return pEdges.map((pEdge) => mermaidEdge(pEdge.from, pEdge.to)).join("\n");
-};
+const renderEdges = (pEdges) =>
+  pEdges.map((pEdge) => renderEdge(pEdge.from, pEdge.to)).join("\n");
 
 /**
  * @param {import('../../types/dependency-cruiser').ICruiseResult} pCruiseResult
  * @param {Map<string, string>} pNamesHashMap
  */
-const convertedEdgeSources = (pCruiseResult, pNamesHashMap) => {
+function convertEdgeSources(pCruiseResult, pNamesHashMap) {
   return pCruiseResult.modules.flatMap((pModule) => {
     const lFromNode = pNamesHashMap.get(pModule.source);
     const lFrom = {
@@ -36,58 +23,55 @@ const convertedEdgeSources = (pCruiseResult, pNamesHashMap) => {
       text: pModule.source.split("/").slice(-1)[0],
     };
 
-    return pModule.dependencies.map((pDep) => {
-      const lToNode = pNamesHashMap.get(pDep.resolved);
+    return pModule.dependencies.map((pDependency) => {
+      const lToNode = pNamesHashMap.get(pDependency.resolved);
       return {
         from: lFrom,
         to: {
           node: lToNode,
-          text: pDep.resolved.split("/").slice(-1)[0],
+          text: pDependency.resolved.split("/").slice(-1)[0],
         },
       };
     });
   });
-};
+}
 
-const indent = (pDepth = 0, pMinify) => {
-  return pMinify ? "" : "  ".repeat(pDepth);
-};
+const indent = (pDepth = 0, pMinify) => (pMinify ? "" : "  ".repeat(pDepth));
 
-const mermaidSubgraph = (pNode, pText, pChildren, pIndent) => {
-  return `${pIndent}subgraph ${mermaidNode(pNode, pText)}
+const renderSubgraph = (pNode, pText, pChildren, pIndent) =>
+  `${pIndent}subgraph ${renderNode(pNode, pText)}
 ${pChildren}
 ${pIndent}end`;
-};
 
-const mermaidSubgraphs = (pSource, pOptions, pDepth = 0) => {
+function renderSubgraphs(pSource, pOptions, pDepth = 0) {
   return Object.keys(pSource)
     .map((pName) => {
       const lIndent = indent(pDepth, pOptions.minify);
       const source = pSource[pName];
-      const children = mermaidSubgraphs(source.children, pOptions, pDepth + 1);
+      const children = renderSubgraphs(source.children, pOptions, pDepth + 1);
       if (children === "")
-        return `${lIndent}${mermaidNode(source.node, source.text)}`;
+        return `${lIndent}${renderNode(source.node, source.text)}`;
 
-      return mermaidSubgraph(source.node, source.text, children, lIndent);
+      return renderSubgraph(source.node, source.text, children, lIndent);
     })
     .join("\n");
-};
+}
 
 /**
  * @param {import('../../types/dependency-cruiser').ICruiseResult} pCruiseResult
- * @param {Map<string, string>} pMinifiedNames
+ * @param {Map<string, string>} pNamesHashMap
  */
-const convertedSubgraphSources = (pCruiseResult, pMinifiedNames) => {
+function convertSubgraphSources(pCruiseResult, pNamesHashMap) {
   let lTree = {};
 
   pCruiseResult.modules.forEach((pModule) => {
-    const paths = pModule.source.split("/");
+    const lPaths = pModule.source.split("/");
 
-    paths.reduce((pChildren, pCurrentPath, pIndex) => {
+    lPaths.reduce((pChildren, pCurrentPath, pIndex) => {
       if (!pChildren[pCurrentPath]) {
-        const node = paths.slice(0, pIndex + 1).join("/");
+        const node = lPaths.slice(0, pIndex + 1).join("/");
         pChildren[pCurrentPath] = {
-          node: pMinifiedNames.get(node),
+          node: pNamesHashMap.get(node),
           text: pCurrentPath,
           children: {},
         };
@@ -97,13 +81,13 @@ const convertedSubgraphSources = (pCruiseResult, pMinifiedNames) => {
   });
 
   return lTree;
-};
+}
 
 /**
  * @param {import("../../types/cruise-result").IModule[]} pModules
  * @param {Map<string, string>} pNamesHashMap
  */
-const focusHighlights = (pModules, pNamesHashMap) => {
+function focusHighlights(pModules, pNamesHashMap) {
   const lHighLightStyle = "fill:lime,color:black";
 
   return pModules
@@ -112,14 +96,21 @@ const focusHighlights = (pModules, pNamesHashMap) => {
       const lSource = pNamesHashMap.get(pModule.source);
       return (pAll += `\nstyle ${lSource} ${lHighLightStyle}`);
     }, "");
-};
+}
+
+const hashToReadableNodeName = (pNode) =>
+  pNode
+    .replace(ACORN_DUMMY_VALUE, "__unknown__")
+    .replace(/^\.$|^\.\//g, "__currentPath__")
+    .replace(/^\.{2}$|^\.{2}\//g, "__prevPath__")
+    .replace(/[[\]/.@~-]/g, "_");
 
 /**
  * @param {import("../../types/cruise-result").IModule[]} pModules
  * @param {Boolean} pMinify
  * @return {Map<string, string>}
  */
-const hashModuleNames = (pModules, pMinify) => {
+function hashModuleNames(pModules, pMinify) {
   const lBase = 36;
   const lNamesHashMap = new Map();
   let lCount = 0;
@@ -129,36 +120,36 @@ const hashModuleNames = (pModules, pMinify) => {
 
     for (let lIndex = 0; lIndex < lPaths.length; lIndex += 1) {
       const lName = lPaths.slice(0, lIndex + 1).join("/");
-      if (!lNamesHashMap.get(lName)) {
+      if (!lNamesHashMap.has(lName)) {
         if (pMinify) {
           lNamesHashMap.set(lName, lCount.toString(lBase));
           lCount += 1;
         } else {
-          lNamesHashMap.set(lName, hashNodeName(lName));
+          lNamesHashMap.set(lName, hashToReadableNodeName(lName));
         }
       }
     }
   });
 
   return lNamesHashMap;
-};
+}
 
 /**
  * @param {import('../../types/dependency-cruiser').ICruiseResult} pCruiseResult
  * @param {import("../../types/reporter-options").IMermaidReporterOptions} pOptions
  */
-const renderMermaidThing = (pCruiseResult, pOptions) => {
+function renderMermaidSource(pCruiseResult, pOptions) {
   const lOptions = { ...REPORT_DEFAULTS, ...(pOptions || {}) };
   const lNamesHashMap = hashModuleNames(pCruiseResult.modules, lOptions.minify);
-  const lSubgraphs = convertedSubgraphSources(pCruiseResult, lNamesHashMap);
-  const lEdges = convertedEdgeSources(pCruiseResult, lNamesHashMap);
+  const lSubgraphs = convertSubgraphSources(pCruiseResult, lNamesHashMap);
+  const lEdges = convertEdgeSources(pCruiseResult, lNamesHashMap);
 
   return `flowchart LR
 
-${mermaidSubgraphs(lSubgraphs, lOptions)}
-${mermaidEdges(lEdges)}
+${renderSubgraphs(lSubgraphs, lOptions)}
+${renderEdges(lEdges)}
 ${focusHighlights(pCruiseResult.modules, lNamesHashMap)}`;
-};
+}
 
 /**
  * mermaid reporter
@@ -168,6 +159,6 @@ ${focusHighlights(pCruiseResult.modules, lNamesHashMap)}`;
  * @return {import('../../types/dependency-cruiser').IReporterOutput}
  */
 module.exports = (pCruiseResult, pOptions) => ({
-  output: renderMermaidThing(pCruiseResult, pOptions),
+  output: renderMermaidSource(pCruiseResult, pOptions),
   exitCode: 0,
 });
--- test/report/mermaid/mermaid.spec.mjs
+++ test/report/mermaid/mermaid.spec.mjs
@@ -10,45 +10,59 @@ const requireJSON = createRequireJSON(import.meta.url);
 const __dirname = fileURLToPath(new URL(".", import.meta.url));
 const mockPath = join(__dirname, "__mocks__");
 
-const same = (pName, pMermaidModule = mermaid) => {
+const same = (pName, pOptions, pMermaidModule = mermaid) => {
   const definition = requireJSON(`./__mocks__/${pName}.json`);
-  const expected = readFileSync(join(mockPath, `${pName}.mmd`), "utf8");
-  const output = pMermaidModule(definition, { minify: false }).output;
-  expect(output).to.deep.equal(expected);
-};
-
-const sameWithMinified = (pName, pMermaidModule = mermaid) => {
-  const definition = requireJSON(`./__mocks__/${pName}.json`);
-  const expected = readFileSync(join(mockPath, `${pName}.min.mmd`), "utf8");
-  const output = pMermaidModule(definition, { minify: true }).output;
+  const expected = readFileSync(
+    join(
+      mockPath,
+      `${pName}${(pOptions || {}).minify === false ? ".mmd" : ".min.mmd"}`
+    ),
+    "utf8"
+  );
+  const output = pMermaidModule(definition, pOptions).output;
   expect(output).to.deep.equal(expected);
 };
 
 describe("[I] report/mermaid", () => {
   it("renders a mermaid - render directories", () => {
-    same("dependency-cruiser-2019-01-14");
-    sameWithMinified("dependency-cruiser-2019-01-14");
+    same("dependency-cruiser-2019-01-14", { minify: false });
+    same("dependency-cruiser-2019-01-14", { minify: true });
   });
   it("renders a mermaid - modules in the root don't come in a cluster", () =>
-    same("clusterless"));
+    same("clusterless", { minify: false }));
   it("renders a mermaid - unresolvable in a sub folder (either existing or not) get labeled as unresolvable", () =>
-    same("es6-unresolvable-deps"));
+    same("es6-unresolvable-deps", { minify: false }));
   it("renders a mermaid - matchesDoNotFollow NOT rendered as folders", () =>
-    same("do-not-follow-deps"));
-  it("renders a mermaid - renders orphan module", () => same("orphan-deps"));
+    same("do-not-follow-deps", { minify: false }));
+  it("renders a mermaid - renders orphan module", () =>
+    same("orphan-deps", { minify: false }));
   it("renders a mermaid - rendered strings that mermaid cannot parse are escaped.", () =>
-    same("contains-strings-to-be-escaped"));
+    same("contains-strings-to-be-escaped", { minify: false }));
   it("renders a mermaid - rendered strings that replaced from unknown figures.", () =>
-    same("unknown-deps"));
-  it("renders a mermaid - renders focussed elements with highlights", () => {
-    same("with-focus");
-    sameWithMinified("with-focus");
+    same("unknown-deps", { minify: false }));
+  it("renders a mermaid - renders focused elements with highlights - unminified", () => {
+    same("with-focus", { minify: false });
+  });
+  it("renders a mermaid - renders focused elements with highlights - no options passed", () => {
+    // eslint-disable-next-line no-undefined
+    same("with-focus", undefined);
+  });
+  it("renders a mermaid - renders focused elements with highlights - minified", () => {
+    same("with-focus", { minify: true });
   });
 });
 
 describe("[I] configs/plugins/mermaid-reporter-plugin", () => {
   it("still renders mermaid with the mermaid reporter as a 'plugin'", () => {
-    same("dependency-cruiser-2019-01-14", mermaidReporterPlugin);
-    sameWithMinified("dependency-cruiser-2019-01-14", mermaidReporterPlugin);
+    same(
+      "dependency-cruiser-2019-01-14",
+      { minify: false },
+      mermaidReporterPlugin
+    );
+    same(
+      "dependency-cruiser-2019-01-14",
+      { minify: true },
+      mermaidReporterPlugin
+    );
   });
 });
