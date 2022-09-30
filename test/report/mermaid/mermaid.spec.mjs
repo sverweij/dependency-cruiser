60f29c9dbcff82b8e1afa9952019acd46c89e46d
Sander Verweij <sverweij@users.noreply.github.com>
2022-09-30 21:52:50 +0200

    refactor(plugins): simplify construct to expose mermaid plugin and stay backword compatible (#670)

--- test/report/mermaid/mermaid.spec.mjs
+++ test/report/mermaid/mermaid.spec.mjs
@@ -2,8 +2,10 @@ import { readFileSync } from "node:fs";
 import { join } from "node:path";
 import { fileURLToPath } from "node:url";
 import { expect } from "chai";
+// eslint-plugins import and node don't yet understand these 'self references'
+// eslint-disable-next-line import/no-unresolved, node/no-missing-import
+import mermaidReporterPlugin from "dependency-cruiser/mermaid-reporter-plugin";
 import mermaid from "../../../src/report/mermaid.js";
-import mermaidReporterPlugin from "../../../configs/plugins/mermaid-reporter-plugin.js";
 import { createRequireJSON } from "../../backwards.utl.mjs";
 
 const requireJSON = createRequireJSON(import.meta.url);
