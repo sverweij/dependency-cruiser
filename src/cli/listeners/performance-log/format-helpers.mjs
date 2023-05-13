a5c2bb56f21fc4a53186349cd1088b0083a76616
sverweij <sverweij@users.noreply.github.com>
2023-05-13 05:53:54 +0200

    feature(progress): some more tweaks to the usability of the performance-log
    - makes message header divider as wide as the messages
    - dims messages of log level >= INFO

--- src/cli/listeners/performance-log/format-helpers.mjs
+++ src/cli/listeners/performance-log/format-helpers.mjs
@@ -1,14 +1,17 @@
+ 
 import chalk from "chalk";
+import { INFO } from "../../../utl/bus.mjs";
 
 const MS_PER_SECOND = 1000;
 const MS_PER_MICRO_SECOND = 0.001;
-const MAX_EXPECTED_LENGTH = 13;
+const MAX_EXPECTED_METRIC_LENGTH = 13;
+const MAX_EXPECTED_MESSAGE_LENGTH = 42;
 const NUMBER_OF_COLUMNS = 8;
 const K = 1024;
 /*
  * using `undefined` as the first parameter to Intl.NumberFormat so
  * it will fall back to the 'current' locale. Using a non-existent language
- * (e.g. `zz`) also works, but `undefined`  seems to be the lesser of the two
+ * (e.g. `zz`) also works, but `undefined` seems to be the lesser of the two
  * evil as it is closer to the intent (skip the optional parameter).
  */
 // eslint-disable-next-line no-undefined
@@ -28,10 +31,16 @@ const gSizeFormat = new Intl.NumberFormat(LOCALE, {
   maximumFractionDigits: 0,
 }).format;
 
-const pad = (pString) => pString.padStart(MAX_EXPECTED_LENGTH).concat(" ");
+const pad = (pString) =>
+  pString.padStart(MAX_EXPECTED_METRIC_LENGTH).concat(" ");
 
 export function formatDividerLine() {
-  return `${`${"-".repeat(MAX_EXPECTED_LENGTH)} `.repeat(NUMBER_OF_COLUMNS)}\n`;
+  const lNumberColumnDivider = "-".repeat(MAX_EXPECTED_METRIC_LENGTH);
+  const lMessageColumnHDivider = "-".repeat(MAX_EXPECTED_MESSAGE_LENGTH);
+
+  return `${`${lNumberColumnDivider} `.repeat(
+    NUMBER_OF_COLUMNS - 1
+  )}${lMessageColumnHDivider}\n`;
 }
 
 export function formatHeader() {
@@ -42,24 +51,40 @@ export function formatHeader() {
         pad("∆ heapTotal") +
         pad("∆ heapUsed") +
         pad("∆ external") +
-        pad("⏱ system") +
-        pad("⏱ user") +
-        pad("⏱ real")
+        pad("⏱  system") +
+        pad("⏱  user") +
+        pad("⏱  real")
       }after step...\n`
     )
     .concat(formatDividerLine());
 }
 
-export function formatTime(pNumber, pConversionMultiplier = MS_PER_SECOND) {
-  return gTimeFormat(pConversionMultiplier * pNumber)
-    .padStart(MAX_EXPECTED_LENGTH)
-    .concat(" ");
+function formatMessage(pMessage, pLevel) {
+  return pLevel >= INFO ? chalk.dim(pMessage) : pMessage;
 }
 
-export function formatMemory(pBytes) {
-  const lReturnValue = gSizeFormat(pBytes / K).padStart(MAX_EXPECTED_LENGTH);
+export function formatTime(
+  pNumber,
+  pConversionMultiplier = MS_PER_SECOND,
+  pLevel
+) {
+  return formatMessage(
+    gTimeFormat(pConversionMultiplier * pNumber)
+      .padStart(MAX_EXPECTED_METRIC_LENGTH)
+      .concat(" "),
+    pLevel
+  );
+}
 
-  return (pBytes < 0 ? chalk.blue(lReturnValue) : lReturnValue).concat(" ");
+export function formatMemory(pBytes, pLevel) {
+  const lReturnValue = gSizeFormat(pBytes / K).padStart(
+    MAX_EXPECTED_METRIC_LENGTH
+  );
+
+  return formatMessage(
+    (pBytes < 0 ? chalk.blue(lReturnValue) : lReturnValue).concat(" "),
+    pLevel
+  );
 }
 
 export function formatPerfLine({
@@ -71,15 +96,16 @@ export function formatPerfLine({
   deltaHeapTotal,
   deltaExternal,
   message,
+  level,
 }) {
   return `${
-    formatMemory(deltaRss) +
-    formatMemory(deltaHeapTotal) +
-    formatMemory(deltaHeapUsed) +
-    formatMemory(deltaExternal) +
-    formatTime(elapsedSystem, MS_PER_MICRO_SECOND) +
-    formatTime(elapsedUser, MS_PER_MICRO_SECOND) +
-    formatTime(elapsedTime) +
-    message
+    formatMemory(deltaRss, level) +
+    formatMemory(deltaHeapTotal, level) +
+    formatMemory(deltaHeapUsed, level) +
+    formatMemory(deltaExternal, level) +
+    formatTime(elapsedSystem, MS_PER_MICRO_SECOND, level) +
+    formatTime(elapsedUser, MS_PER_MICRO_SECOND, level) +
+    formatTime(elapsedTime, MS_PER_SECOND, level) +
+    formatMessage(message, level)
   }\n`;
 }
