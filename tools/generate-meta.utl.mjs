5d6d4b323ed92d6fe99516b2b6acb8a45b655602
sverweij <sverweij@users.noreply.github.com>
2023-05-27 11:47:46 +0200

    chore(tools): updates meta.js generator to use top level await

--- tools/generate-meta.utl.mjs
+++ tools/generate-meta.utl.mjs
@@ -1,16 +1,16 @@
 import prettier from "prettier";
 import getStream from "get-stream";
 
-getStream(process.stdin).then((pJSONAsString) => {
-  const $package = JSON.parse(pJSONAsString);
-  const lGeneratedSource = `/* generated - don't edit */
+const lJSONAsString = await getStream(process.stdin);
+const $package = JSON.parse(lJSONAsString);
+const lGeneratedSource = `/* generated - don't edit */
   
   module.exports = {
-      version: "${$package.version}",
-      engines: {
-        node: "${$package.engines.node}",
-      },
-      supportedTranspilers: ${JSON.stringify($package.supportedTranspilers)}
-      }`;
-  console.log(prettier.format(lGeneratedSource, { parser: "babel" }));
-});
+    version: "${$package.version}",
+    engines: {
+      node: "${$package.engines.node}",
+    },
+    supportedTranspilers: ${JSON.stringify($package.supportedTranspilers)}
+  }`;
+
+console.log(prettier.format(lGeneratedSource, { parser: "babel" }));
