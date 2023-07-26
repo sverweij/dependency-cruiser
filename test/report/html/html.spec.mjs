7688ea07cf5820e877482632f63ab52bc1e7cfa9
sverweij <sverweij@users.noreply.github.com>
2023-07-26 21:00:24 +0200

    refactor(test/report/html): replaces chai with node:assert

--- test/report/html/html.spec.mjs
+++ test/report/html/html.spec.mjs
@@ -1,6 +1,6 @@
 import { readFileSync } from "node:fs";
 import { fileURLToPath } from "node:url";
-import { expect } from "chai";
+import { deepStrictEqual, strictEqual } from "node:assert";
 import render from "../../../src/report/html/index.mjs";
 import deps from "./__mocks__/cjs-no-dependency-valid.mjs";
 
@@ -15,7 +15,7 @@ describe("[I] report/html reporter", () => {
   it("renders html - modules in the root don't come in a cluster; and one module could not be resolved", () => {
     const lReturnValue = render(deps);
 
-    expect(lReturnValue.output).to.deep.equal(elementFixture);
-    expect(lReturnValue.exitCode).to.equal(0);
+    deepStrictEqual(lReturnValue.output, elementFixture);
+    strictEqual(lReturnValue.exitCode, 0);
   });
 });
