4a3b8cf122a6958bd5c71f841e24562caced3b62
sverweij <sverweij@users.noreply.github.com>
2023-08-05 18:21:34 +0200

    fix: cleans up a forgotten test fixture

--- test/extract/__mocks__/es6/imports-and-exports.js
+++ test/extract/__mocks__/es6/imports-and-exports.js
@@ -1,8 +1,6 @@
-import {parse} from "acorn";
-import * as tea from "chai";
-export * from "mocha";
-export {Linter} from "eslint";
+import { parse } from "acorn";
+export * as mocha_but_renamed from "mocha";
+export { Linter } from "eslint";
 import "os";
-
 
 export const version = "1.0.0";
