64eb79ddcec16c4ed55d61250618dbd6a8c5f17f
sverweij <sverweij@users.noreply.github.com>
2023-12-06 13:00:05 +0100

    feat(report/dot): adds ability to match arrays for conditional coloring

--- src/report/dot/theming.mjs
+++ src/report/dot/theming.mjs
@@ -8,13 +8,42 @@ function matchesRE(pValue, pRE) {
   return Boolean(lMatchResult) && lMatchResult.length > 0;
 }
 
+function matchesCriterion(pModuleKey, pCriterion) {
+  return pModuleKey === pCriterion || matchesRE(pModuleKey, pCriterion);
+}
+
 function moduleOrDependencyMatchesCriteria(pSchemeEntry, pModule) {
-  return Object.keys(pSchemeEntry.criteria).every(
-    (pKey) =>
-      (get(pModule, pKey) || has(pModule, pKey)) &&
-      (get(pModule, pKey) === get(pSchemeEntry.criteria, pKey) ||
-        matchesRE(get(pModule, pKey), get(pSchemeEntry.criteria, pKey))),
-  );
+  return Object.keys(pSchemeEntry.criteria).every((pKey) => {
+    // we use lodash.get here because in the criteria you can enter
+    // nested keys like "rules[0].severity" : "error", and lodash.get handles
+    // that for us
+    const lCriterion = get(pSchemeEntry.criteria, pKey);
+    const lModuleKey = get(pModule, pKey);
+
+    if (!(lModuleKey || has(pModule, pKey))) {
+      return false;
+    }
+
+    if (Array.isArray(lModuleKey)) {
+      if (Array.isArray(lCriterion)) {
+        return lCriterion.some((pCriterionEntry) =>
+          lModuleKey.some((pModuleKeyEntry) =>
+            matchesCriterion(pModuleKeyEntry, pCriterionEntry),
+          ),
+        );
+      } else {
+        return lModuleKey.some((pModuleKeyEntry) =>
+          matchesCriterion(pModuleKeyEntry, lCriterion),
+        );
+      }
+    }
+    if (Array.isArray(lCriterion)) {
+      return lCriterion.some((pCriterionEntry) =>
+        matchesCriterion(lModuleKey, pCriterionEntry),
+      );
+    }
+    return matchesCriterion(lModuleKey, lCriterion);
+  });
 }
 
 function determineAttributes(pModuleOrDependency, pAttributeCriteria) {
