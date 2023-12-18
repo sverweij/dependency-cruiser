d8d8782c41f18e565e03bfc66476e0256dc6e285
sverweij <sverweij@users.noreply.github.com>
2023-12-18 10:47:22 +0100

    feat(config-utl): makes possibly old format known violations forward compatible with the new one

--- test/config-utl/extract-known-violations.spec.mjs
+++ test/config-utl/extract-known-violations.spec.mjs
@@ -55,4 +55,116 @@ describe("[I] config-utl/extractKnownViolations", () => {
       },
     ]);
   });
+
+  it("Makes a forward compatible version of the known violations", async () => {
+    process.chdir("./test/config-utl/__mocks__/known-violations");
+    deepEqual(
+      await extractKnownViolations(
+        "known-violations-with-cycles-in-old-format.json",
+      ),
+      [
+        {
+          type: "cycle",
+          from: "tmp-errors/cycle-1.js",
+          to: "tmp-errors/cycle-2.js",
+          rule: {
+            severity: "error",
+            name: "no-circular",
+          },
+          cycle: [
+            {
+              name: "tmp-errors/cycle-2.js",
+              dependencyTypes: [],
+            },
+            {
+              name: "tmp-errors/cycle-3.js",
+              dependencyTypes: [],
+            },
+            {
+              name: "tmp-errors/cycle-4.js",
+              dependencyTypes: [],
+            },
+            {
+              name: "tmp-errors/cycle-1.js",
+              dependencyTypes: [],
+            },
+          ],
+        },
+        {
+          type: "module",
+          from: "tmp-errors/mod.mts",
+          to: "tmp-errors/mod.mts",
+          rule: {
+            severity: "error",
+            name: "no-orphans",
+          },
+        },
+        {
+          type: "dependency",
+          from: "tmp-errors/call-mod.mts",
+          to: "./mod.mjs",
+          rule: {
+            severity: "error",
+            name: "not-to-unresolvable",
+          },
+        },
+      ],
+    );
+  });
+
+  it("Leaves new format cycles alone", async () => {
+    process.chdir("./test/config-utl/__mocks__/known-violations");
+    deepEqual(
+      await extractKnownViolations(
+        "known-violations-with-cycles-in-new-format.json",
+      ),
+      [
+        {
+          type: "cycle",
+          from: "tmp-errors/cycle-1.js",
+          to: "tmp-errors/cycle-2.js",
+          rule: {
+            severity: "error",
+            name: "no-circular",
+          },
+          cycle: [
+            {
+              name: "tmp-errors/cycle-2.js",
+              dependencyTypes: ["local", "require"],
+            },
+            {
+              name: "tmp-errors/cycle-3.js",
+              dependencyTypes: ["local", "require"],
+            },
+            {
+              name: "tmp-errors/cycle-4.js",
+              dependencyTypes: ["local", "require"],
+            },
+            {
+              name: "tmp-errors/cycle-1.js",
+              dependencyTypes: ["local", "require"],
+            },
+          ],
+        },
+        {
+          type: "module",
+          from: "tmp-errors/mod.mts",
+          to: "tmp-errors/mod.mts",
+          rule: {
+            severity: "error",
+            name: "no-orphans",
+          },
+        },
+        {
+          type: "dependency",
+          from: "tmp-errors/call-mod.mts",
+          to: "./mod.mjs",
+          rule: {
+            severity: "error",
+            name: "not-to-unresolvable",
+          },
+        },
+      ],
+    );
+  });
 });
