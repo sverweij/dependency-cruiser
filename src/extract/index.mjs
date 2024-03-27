8053932cddd3d98b8a83374463cf9daecdaf4e85
sverweij <sverweij@users.noreply.github.com>
2024-03-27 20:46:43 +0100

    feat(extract): adds first sketch to add basic stats from the parsed  AST's

--- src/extract/index.mjs
+++ src/extract/index.mjs
@@ -1,9 +1,10 @@
 import extractDependencies from "./extract-dependencies.mjs";
+import extractStats from "./extract-stats.mjs";
 import gatherInitialSources from "./gather-initial-sources.mjs";
 import clearCaches from "./clear-caches.mjs";
 import { bus } from "#utl/bus.mjs";
 
-/* eslint max-params:0 */
+/* eslint max-params:0 , max-lines-per-function:0*/
 function extractRecursive(
   pFileName,
   pCruiseOptions,
@@ -47,6 +48,15 @@ function extractRecursive(
         {
           source: pFileName,
           dependencies: lDependencies,
+          ...(pCruiseOptions.experimentalStats
+            ? {
+                experimentalStats: extractStats(
+                  pFileName,
+                  pCruiseOptions,
+                  pTranspileOptions,
+                ),
+              }
+            : {}),
         },
       ],
     );
