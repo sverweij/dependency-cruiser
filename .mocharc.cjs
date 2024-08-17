0db2f80b904bd22096b4a53709e450fe826ed3a4
sverweij <sverweij@users.noreply.github.com>
2024-08-17 11:44:21 +0200

    baby-sit windows

--- .mocharc.cjs
+++ .mocharc.cjs
@@ -3,6 +3,6 @@
 module.exports = {
   extension: ["js", "mjs", "cjs"],
   reporter: "dot",
-  timeout: 4000,
+  timeout: 8000, // 8s for testing on windows - with the previous setting of 4s it regularly timed out on the "uses the 'dot' reporter section for the 'x-dot-webpage' output type" test in main.format
   spec: "test/**/*.spec.{js,mjs,cjs}",
 };
