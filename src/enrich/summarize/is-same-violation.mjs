844839af99ee7485df92257e94a53991cb21362b
sverweij <sverweij@users.noreply.github.com>
2025-06-22 14:27:03 +0200

    fix(enrich): enables ignoring reachability (, instability) validations as well

--- src/enrich/summarize/is-same-violation.mjs
+++ src/enrich/summarize/is-same-violation.mjs
@@ -3,22 +3,31 @@ function pluckName({ name }) {
 }
 
 export default function isSameViolation(pLeftViolation, pRightViolation) {
-  let lReturnValue = false;
-
   if (pLeftViolation.rule.name === pRightViolation.rule.name) {
     if (pRightViolation.cycle && pLeftViolation.cycle) {
-      lReturnValue =
+      return (
         pLeftViolation.cycle.length === pRightViolation.cycle.length &&
         pLeftViolation.cycle
           .map(pluckName)
           .every((pModule) =>
             pRightViolation.cycle.map(pluckName).includes(pModule),
-          );
-    } else {
-      lReturnValue =
-        pLeftViolation.from === pRightViolation.from &&
-        pLeftViolation.to === pRightViolation.to;
+          )
+      );
     }
+    if (pRightViolation.via && pLeftViolation.via) {
+      return (
+        pLeftViolation.via.length === pRightViolation.via.length &&
+        pLeftViolation.via
+          .map(pluckName)
+          .every((pModule) =>
+            pRightViolation.via.map(pluckName).includes(pModule),
+          )
+      );
+    }
+    return (
+      pLeftViolation.from === pRightViolation.from &&
+      pLeftViolation.to === pRightViolation.to
+    );
   }
-  return lReturnValue;
+  return false;
 }
