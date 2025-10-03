78a21c8b06995a1ed955b3f3d4e7d0da7dd04ba7
sverweij <sverweij@users.noreply.github.com>
2025-10-03 12:10:30 +0200

    adds automated non-regression test

---
+++ test/cli/__fixtures__/typescriptconfig/cli-config-with-path-and-extensions/src/index.ts
@@ -0,0 +1,6 @@
+import * as shared from 'shared/asneeze';
+import else from "./something/else";
+
+console.log(
+    shared.version
+)
---
+++ test/cli/__fixtures__/typescriptconfig/cli-config-with-path-and-extensions/src/something/else.ts
@@ -0,0 +1,7 @@
+import * as shared from 'shared/asneeze';
+
+console.log(
+    'from someting else', shared.version
+)
+
+export default 'else';
