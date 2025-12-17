7aad83e53b3f6801b9a83da591e18acb95492fd5
sverweij <sverweij@users.noreply.github.com>
2025-12-17 22:16:47 +0100

    perf(enrich/derive/metrics): only create indexed graph once

--- src/enrich/derive/metrics/index.mjs
+++ src/enrich/derive/metrics/index.mjs
@@ -2,12 +2,16 @@ import {
   addInstabilityMetric,
   deNormalizeInstabilityMetricsToDependencies,
 } from "./get-module-metrics.mjs";
+import IndexedModuleGraph from "#graph-utl/indexed-module-graph.mjs";
 
 export default function deriveModulesMetrics(pModules, pOptions) {
   if (pOptions.metrics) {
-    return pModules
-      .map(addInstabilityMetric)
-      .map(deNormalizeInstabilityMetricsToDependencies);
+    const lModules = pModules.map(addInstabilityMetric);
+    const lIndexedModules = new IndexedModuleGraph(lModules);
+
+    return lModules.map((pModule) =>
+      deNormalizeInstabilityMetricsToDependencies(pModule, lIndexedModules),
+    );
   }
   return pModules;
 }
