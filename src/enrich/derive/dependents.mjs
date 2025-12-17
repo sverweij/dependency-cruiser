511f2910bb400c10f127f9b9102f540edf99225b
sverweij <sverweij@users.noreply.github.com>
2025-12-17 20:08:46 +0100

    perf(enrich/derive): uses better datastructure for deriving dependents

--- src/enrich/derive/dependents.mjs
+++ src/enrich/derive/dependents.mjs
@@ -1,5 +1,3 @@
-import { isDependent } from "./module-utl.mjs";
-
 /** @import { IFlattenedRuleSet } from "../../../types/rule-set.mjs" */
 
 function isDependentsRule(pRule) {
@@ -7,7 +5,8 @@ function isDependentsRule(pRule) {
   // the 'metrics' flag in options, sot not going to repeat that here
 
   // dependents are used in the orphans analsys. However, there is a fall back
-  // where it does its own analysis, so not going to repeat that check here.
+  // where it does its own analysis which is faster on itself, so not going
+  // to repeat that check here either.
   return (
     /* c8 ignore start */
     Object.hasOwn(pRule?.module ?? {}, "numberOfDependentsLessThan") ||
@@ -16,10 +15,10 @@ function isDependentsRule(pRule) {
   );
 }
 
-export function getDependents(pModule, pModules) {
+export function getDependents(pModule, pModulesWithDependencySet) {
   // perf between O(n) in an unconnected graph and O(n^2) in a fully connected one
-  return pModules
-    .filter(isDependent(pModule.source))
+  return pModulesWithDependencySet
+    .filter(({ dependencies }) => dependencies.has(pModule.source))
     .map((pDependentModule) => pDependentModule.source);
 }
 
@@ -39,9 +38,19 @@ export default function addDependents(
   { skipAnalysisNotInRules, metrics, ruleSet },
 ) {
   if (!skipAnalysisNotInRules || metrics || hasDependentsRule(ruleSet)) {
+    // creating this optimized structure here might seem overkill, but on
+    // large graphs it pays off significantly - creation is a few centiseconds
+    // but it cuts analysis in half on large graphs (and even on smaller
+    // graphs it's a win, even though just a few milliseconds)
+    const lModulesWithDependencySet = pModules.map((pModule) => ({
+      source: pModule.source,
+      dependencies: new Set(
+        pModule.dependencies.map((pDependency) => pDependency.resolved),
+      ),
+    }));
     return pModules.map((pModule) => ({
       ...pModule,
-      dependents: getDependents(pModule, pModules),
+      dependents: getDependents(pModule, lModulesWithDependencySet),
     }));
   }
   return pModules;
