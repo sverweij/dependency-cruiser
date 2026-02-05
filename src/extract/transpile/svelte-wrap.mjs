153cef6341cca850b6926a87570097c6f9807a2a
sverweij <sverweij@users.noreply.github.com>
2026-02-05 21:18:36 +0100

    fix(extract/transpile): retain svelte dependencies used exclusively outside <script> sections

--- src/extract/transpile/svelte-wrap.mjs
+++ src/extract/transpile/svelte-wrap.mjs
@@ -1,3 +1,4 @@
+/* eslint-disable no-inline-comments */
 import preProcess from "./svelte-preprocess.mjs";
 import tryImport from "#utl/try-import.mjs";
 import meta from "#meta.cjs";
@@ -6,14 +7,19 @@ const { compile, VERSION } = await tryImport(
   "svelte/compiler",
   meta.supportedTranspilers.svelte,
 );
+/* c8 ignore next */
+const MAJOR_VERSION = VERSION ? Number(VERSION.split(".")[0]) : 0;
 
 function getTranspiler(pTranspilerWrapper) {
   return (pSource, _pFileName, pTranspilerOptions) => {
-    const lPreProcessedSource = preProcess(
-      pSource,
-      pTranspilerWrapper,
-      pTranspilerOptions,
-    );
+    // svelte 5 natively supports typescript directly, without
+    // the need for preprocessing.
+    const lPreProcessedSource =
+      // eslint-disable-next-line no-magic-numbers
+      MAJOR_VERSION < 5
+        ? /* c8 ignore next */
+          preProcess(pSource, pTranspilerWrapper, pTranspilerOptions)
+        : pSource;
     // in svelte 5 one must provide the second argument
     // lest it throws because it's accessing a property
     // in it. In svelte 4 (which also takes compiler
