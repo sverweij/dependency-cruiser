f659dd75e8bb825832969eebed7b94663d5cbfec
sverweij <sverweij@users.noreply.github.com>
2026-07-11 22:16:24 +0200

    fix(test): ensures the new 'environment' is ignored when comparing result-json

--- test/cli/asserthelpers.utl.mjs
+++ test/cli/asserthelpers.utl.mjs
@@ -3,6 +3,18 @@ import { deepEqual, equal } from "node:assert/strict";
 import normalizeNewline from "normalize-newline";
 import normBaseDirectory from "../main/norm-base-directory.utl.mjs";
 
+const DUMMY_ENVIRONMENT = {
+  version: "481",
+  nodeVersionSupported: "^42",
+  nodeVersionFound: "42.1.2",
+  osVersionFound: `riscv pinecil@1.2.3`,
+  transpilersFound: [],
+  extensionsFound: [],
+};
+
+const neutralizeEnvironment = (pKey, pValue) =>
+  pKey === "environment" ? DUMMY_ENVIRONMENT : pValue;
+
 export function assertFileEqual(pActualFileName, pExpectedFileName) {
   equal(
     normalizeNewline(readFileSync(pActualFileName, { encoding: "utf8" })),
@@ -11,9 +23,15 @@ export function assertFileEqual(pActualFileName, pExpectedFileName) {
 }
 export function assertJSONFileEqual(pActualFileName, pExpectedFileName) {
   deepEqual(
-    JSON.parse(readFileSync(pActualFileName, { encoding: "utf8" })),
+    JSON.parse(
+      readFileSync(pActualFileName, { encoding: "utf8" }),
+      neutralizeEnvironment,
+    ),
     normBaseDirectory(
-      JSON.parse(readFileSync(pExpectedFileName, { encoding: "utf8" })),
+      JSON.parse(
+        readFileSync(pExpectedFileName, { encoding: "utf8" }),
+        neutralizeEnvironment,
+      ),
     ),
   );
 }
