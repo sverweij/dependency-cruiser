2dbdc2dc271fd8d75081c540a15ffd10ba09243e
sverweij <sverweij@users.noreply.github.com>
2026-09-17 20:22:57 +0200

    Revert "fix: address requested baseline review feedback"
    This reverts commit f0af06f112281bc15ba903b2451b8f7ef2925b21.

--- src/cli/index.mjs
+++ src/cli/index.mjs
@@ -97,13 +97,6 @@ function setUpListener(pCruiseOptions) {
   }
 }
 
-function isBaselineViewMode(pCruiseOptions) {
-  return (
-    pCruiseOptions.outputType === "baseline" &&
-    pCruiseOptions?.baseline?.mode === "view"
-  );
-}
-
 async function runCruise(pFileDirectoryArray, pCruiseOptions, pErrorStream) {
   const lCruiseOptions = await addKnownViolations(
     await normalizeCliOptions(pCruiseOptions, pErrorStream),
@@ -133,11 +126,9 @@ async function runCruise(pFileDirectoryArray, pCruiseOptions, pErrorStream) {
     { tsConfig, babelConfig },
   );
 
-  if (!isBaselineViewMode(lCruiseOptions)) {
-    bus.progress("cli: writing results", { complete: 1 });
-    bus.emit("write-start");
-    write(lCruiseOptions.outputTo, lReportingResult.output);
-  }
+  bus.progress("cli: writing results", { complete: 1 });
+  bus.emit("write-start");
+  write(lCruiseOptions.outputTo, lReportingResult.output);
   if (lReportingResult.meta) {
     pErrorStream.write(`${lReportingResult.meta}${EOL}`);
   }
